57dc98f604cf06aa5aff3d42bce1af66597d1e01
gdh <gdh1@hi.is>
2017-10-03 14:17:25 +0000

    Updating as newer version of PHPCS has been released,

--- tools-init.sh
+++ tools-init.sh
@@ -4,16 +4,16 @@ if [ ! -d ~/vip-go-ci-tools ] ; then
 	TMP_FOLDER=`mktemp -d /tmp/vip-go-ci-tools-XXXXXX`
 
 	cd $TMP_FOLDER && \
-	wget https://github.com/squizlabs/PHP_CodeSniffer/archive/2.8.0.tar.gz && \
-	tar -zxvf 2.8.0.tar.gz  && \
-	rm -fv 2.8.0.tar.gz && \
-	mv PHP_CodeSniffer-2.8.0/ phpcs && \
-	wget https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/archive/0.11.0.tar.gz && \
-	tar -zxvf 0.11.0.tar.gz  && \
-	rm -fv 0.11.0.tar.gz && \
-	mv WordPress-Coding-Standards-0.11.0/WordPress* phpcs/CodeSniffer/Standards/ && \
+	wget https://github.com/squizlabs/PHP_CodeSniffer/archive/3.1.0.tar.gz && \
+	tar -zxvf 3.1.0.tar.gz  && \
+	rm -fv 3.1.0.tar.gz && \
+	mv PHP_CodeSniffer-3.1.0/ phpcs && \
+	wget https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/archive/0.13.1.tar.gz && \
+	tar -zxvf 0.13.1.tar.gz  && \
+	rm -fv 0.13.1.tar.gz && \
+	mv WordPress-Coding-Standards-0.13.1/WordPress* phpcs/src/Standards/ && \
 	git clone -b master https://github.com/Automattic/VIP-Coding-Standards.git VIP-Coding-Standards && \
-	mv VIP-Coding-Standards/WordPressVIPMinimum/ phpcs/CodeSniffer/Standards/  && \
+	mv VIP-Coding-Standards/WordPressVIPMinimum/ phpcs/src/Standards/  && \
 	git clone -b master https://github.com/Automattic/vip-go-ci.git && \
 	mv $TMP_FOLDER ~/vip-go-ci-tools && \
 	echo "Installation finished"
