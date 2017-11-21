d57dc3e32c671a66399775c57bee14e2830d6dfd
gdh <gdh1@hi.is>
2017-11-21 17:59:44 +0000

    Print out contents of local repository

--- vip-go-ci.php
+++ vip-go-ci.php
@@ -307,6 +307,8 @@ function vipgoci_run() {
 		'/'
 	);
 
+	exec( "find " . $options['local-git-repo'], $arr ); print_r( $arr); die(" --");
+
 	vipgoci_github_repo_ok(
 		$options['commit'],
 		$options['local-git-repo']
