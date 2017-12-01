1ac51ae2112012fee1af8612a8a8d83588f6fccc
gdh <gdh1@hi.is>
2017-12-01 22:36:30 +0000

    Removing debugging-code

--- github-api.php
+++ github-api.php
@@ -1397,8 +1397,6 @@ function vipgoci_github_prs_implicated(
 			)
 		);
 
-		print_r($github_url);
-		print_r($prs_implicated_unfiltered);
 
 		foreach ( $prs_implicated_unfiltered as $pr_item ) {
 			if ( ! isset( $pr_item->head->ref ) ) {
--- vip-go-ci.php
+++ vip-go-ci.php
@@ -308,7 +308,6 @@ function vipgoci_run() {
 		'/'
 	);
 
-	exec( "find " . $options['local-git-repo'], $arr ); print_r( $arr);
 
 	vipgoci_gitrepo_ok(
 		$options['commit'],
