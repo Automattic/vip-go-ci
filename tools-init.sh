b01ed11dc2a5cdd023d41817aed79a9bd6ab7ce1
gdh <gdh1@hi.is>
2017-12-15 12:56:17 +0000

    Use version numbers for all packages, reduces chances of things breaking

--- tools-init.sh
+++ tools-init.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+export PHP_CODENSIFFER_VER="3.1.0"
+export WP_CODING_STANDARDS_VER="0.14.0"
+export VIP_CODING_STANDARDS_VER="0.2.1"
+
 if [ ! -d ~/vip-go-ci-tools ] ; then
 	TMP_FOLDER=`mktemp -d /tmp/vip-go-ci-tools-XXXXXX`
 
 	cd $TMP_FOLDER && \
-	wget https://github.com/squizlabs/PHP_CodeSniffer/archive/3.1.0.tar.gz && \
-	tar -zxvf 3.1.0.tar.gz  && \
-	rm -fv 3.1.0.tar.gz && \
-	mv PHP_CodeSniffer-3.1.0/ phpcs && \
-	wget https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/archive/0.14.0.tar.gz && \
-	tar -zxvf 0.14.0.tar.gz  && \
-	rm -fv 0.14.0.tar.gz && \
-	mv WordPress-Coding-Standards-0.14.0/WordPress* phpcs/src/Standards/ && \
-	wget https://github.com/Automattic/VIP-Coding-Standards/archive/master.tar.gz && \
-	tar -zxvf master.tar.gz && \
-	mv VIP-Coding-Standards-master/WordPressVIPMinimum/ phpcs/src/Standards/  && \
-	rm -f master.tar.gz && \
+	wget "https://github.com/squizlabs/PHP_CodeSniffer/archive/$PHP_CODENSIFFER_VER.tar.gz" && \
+	tar -zxvf "$PHP_CODENSIFFER_VER.tar.gz"  && \
+	rm -fv "$PHP_CODENSIFFER_VER.tar.gz" && \
+	mv "PHP_CodeSniffer-$PHP_CODENSIFFER_VER/" phpcs && \
+	wget "https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/archive/$WP_CODING_STANDARDS_VER.tar.gz" && \
+	tar -zxvf "$WP_CODING_STANDARDS_VER.tar.gz"  && \
+	rm -fv "$WP_CODING_STANDARDS_VER.tar.gz" && \
+	mv WordPress-Coding-Standards-$WP_CODING_STANDARDS_VER/WordPress* phpcs/src/Standards/ && \
+	wget "https://github.com/Automattic/VIP-Coding-Standards/archive/$VIP_CODING_STANDARDS_VER.tar.gz" && \
+	tar -zxvf "$VIP_CODING_STANDARDS_VER.tar.gz" && \
+	mv "VIP-Coding-Standards-$VIP_CODING_STANDARDS_VER/WordPressVIPMinimum/" phpcs/src/Standards/  && \
+	rm -f "$VIP_CODING_STANDARDS_VER".tar.gz && \
 	wget https://github.com/Automattic/vip-go-ci/archive/master.tar.gz && \
 	tar -zxvf master.tar.gz && \
 	mv vip-go-ci-master vip-go-ci && \
