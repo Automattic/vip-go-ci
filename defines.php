93b03f0a84dc284eaa8c112d0dda9c7359db7709
Gudmundur Haraldsson <gdh1@hi.is>
2018-07-02 16:42:26 +0000

    Adding defines file

---
+++ defines.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Client-ID for curl-requests, etc.
+ */
+
+define( 'VIPGOCI_CLIENT_ID', 'automattic-vip-go-ci' );
+define( 'VIPGOCI_SYNTAX_ERROR_STR', 'PHP Syntax Errors Found' );
+define( 'VIPGOCI_GITHUB_ERROR_STR', 'GitHub API communication error');
+
+
+/*
+ * Define exit-codes
+ */
+
+define( 'VIPGOCI_EXIT_NORMAL',		0 );
+define( 'VIPGOCI_EXIT_CODE_ISSUES',	250 );
+define( 'VIPGOCI_EXIT_SYSTEM_PROBLEM',	251 );
+define( 'VIPGOCI_EXIT_GITHUB_PROBLEM',	252 );
+define( 'VIPGOCI_EXIT_USAGE_ERROR',	253 );
+
+
+/*
+ * Define statistics-types.
+ *
+ * Note: These are related to the command-line
+ * arguments passed to the program (e.g., --phpcs)
+ * -- altering these is not recommended.
+ */
+
+define( 'VIPGOCI_STATS_PHPCS',		'phpcs'		);
+define( 'VIPGOCI_STATS_LINT',		'lint'		);
+define( 'VIPGOCI_STATS_HASHES_API',	'hashes-api'	);
