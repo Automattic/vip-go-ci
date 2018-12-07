cf00822517cca55e75818d6bd6a34e7e3bbfd4d3
Gudmundur Haraldsson <gdh1@hi.is>
2018-12-07 14:06:04 +0000

    Upgrading to latest versions of tools.

--- tools-init.sh
+++ tools-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PHP_CODESNIFFER_VER="3.3.1"
-export WP_CODING_STANDARDS_VER="1.0.0"
-export VIP_CODING_STANDARDS_VER="0.3.0"
+export PHP_CODESNIFFER_VER="3.3.2"
+export WP_CODING_STANDARDS_VER="1.2.0"
+export VIP_CODING_STANDARDS_VER="0.3.1"
 
 export TMP_LOCK_FILE="$HOME/.vip-go-ci-tools-init.lck"
 
