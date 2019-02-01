a4d16eaadf6a7ee397a5ba985f20116af4b2d268
Gudmundur Haraldsson <gdh1@hi.is>
2019-02-01 15:30:57 +0000

    More tests

--- tests/stats_init_test.php
+++ tests/stats_init_test.php
@@ -7,6 +7,9 @@ require( __DIR__ . '/../defines.php' );
 require( __DIR__ . '/../statistics.php' );
 
 final class StatsTests extends TestCase {
+	/*
+	 * Test vipgoci_stats_init(
+	 */
 	public function testStatsInit() {
 		$pr_item1 = new stdClass();
 		$pr_item1->number = 100;
@@ -74,6 +77,120 @@ final class StatsTests extends TestCase {
 			$stats_arr
 		);
 	}
-}
+
+	/*
+	 * Test vipgoci_runtime_measure()
+	 */
+	function testRuntimeMeasure1() {
+		return $this->assertEquals(
+			vipgoci_runtime_measure( 'illegalaction', 'mytimer1' ),
+			false
+		);
+	}
+
+	function testRuntimeMeasure2() {
+		return $this->assertEquals(
+			vipgoci_runtime_measure( VIPGOCI_RUNTIME_STOP, 'mytimer2' ),
+			false
+		);
+	}
+
+	function testRuntimeMeasure3() {
+		$this->assertEquals(
+			vipgoci_runtime_measure( VIPGOCI_RUNTIME_START, 'mytimer3' ),
+			true
+		);
+
+		sleep( 2 );
+
+		$this->assertGreaterThanOrEqual(
+			1,
+			vipgoci_runtime_measure( VIPGOCI_RUNTIME_STOP, 'mytimer3' )
+		);
+	
+		$runtime_stats = vipgoci_runtime_measure(
+			VIPGOCI_RUNTIME_DUMP
+		);
+
+		$this->assertGreaterThanOrEqual(
+			1,
+			$runtime_stats[ 'mytimer3' ]
+		);
+	}
+
+	function testRuntimeMeasure4() {
+		$this->assertEquals(
+			vipgoci_runtime_measure( VIPGOCI_RUNTIME_START, 'mytimer4' ),
+			true
+		);
+
+		sleep( 2 );
+
+		$runtime_stats = vipgoci_runtime_measure(
+			VIPGOCI_RUNTIME_DUMP
+		);
+
+		$this->assertGreaterThanOrEqual(
+			1,
+			$runtime_stats[ 'mytimer3' ]
+		);
+
+	}
+
+
+	/*
+	 * Test vipgoci_counter_report()
+	 */
+
+}	function testCounterReport1() {
+		$this->assertEqual(
+			vipgoci_counter_report(
+				'illegalaction',
+				'mycounter1',
+				100
+			),
+			false
+		);
+
+		$this->assertEqual(
+			vipgoci_counter_report(
+				VIPGOCI_COUNTERS_DUMP
+			),
+			array()
+		);
+	}
+
+	function testCounterReport2() {
+		$this->assertEqual(
+			vipgoci_counter_report(
+				VIPGOCI_COUNTERS_DO,
+				'mycounter2',
+				100
+			),
+			true
+		);
+
+		$this->assertEqual(
+			vipgoci_counter_report(
+				VIPGOCI_COUNTERS_DO,
+				'mycounter2',
+				1
+			),
+			true
+		);
+
+		$this->assertEqual(
+			vipgoci_counter_report(
+				VIPGOCI_COUNTERS_DUMP
+			),
+			array(
+				'mycounter2' => 101,
+			)
+		);
+	}
+
+
+
+
 
 
