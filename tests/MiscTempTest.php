3ff0627f331c56d2de14d97e7b9c3fd49d2d44df
Gudmundur Haraldsson <gdh1@hi.is>
2019-02-12 13:45:29 +0000

    Adding tests for temporary files.

---
+++ tests/MiscTempTest.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once( __DIR__ . '/../defines.php' );
+require_once( __DIR__ . '/../misc.php' );
+require_once( __DIR__ . '/../statistics.php' );
+
+
+final class MiscTempTest extends TestCase {
+	/**
+	 * @covers ::vipgoci_file_extension
+	 */
+	public function testTempFile1() {
+		$file_name_prefix = 'myfilename1';
+		$file_name_extension = null;
+		$file_contents = 'mycontentsofthefile1' . PHP_EOL;
+
+		$temp_file_name = vipgoci_save_temp_file(
+			$file_name_prefix,
+			$file_name_extension,
+			$file_contents
+		);
+
+		$this->assertNotEquals(
+			$temp_file_name,
+			false
+		);
+
+        
+		$temp_file_extension = pathinfo(
+			$temp_file_name,
+			PATHINFO_EXTENSION
+		);
+
+		$temp_file_contents = file_get_contents(
+			$temp_file_name
+		);
+
+		$this->assertEquals(
+			$temp_file_extension,
+			$file_name_extension
+		);
+
+		$this->assertEquals(
+			$file_contents,
+			$temp_file_contents
+		);
+
+		unlink( $temp_file_name );
+	}
+
+	/**
+	 * @covers ::vipgoci_file_extension
+	 */
+	public function testTempFile2() {
+		$file_name_prefix = 'myfilename2';
+		$file_name_extension = 'txt';
+		$file_contents = 'mycontentsofthefile2' . PHP_EOL;
+
+		$temp_file_name = vipgoci_save_temp_file(
+			$file_name_prefix,
+			$file_name_extension,
+			$file_contents
+		);
+
+		$this->assertNotEquals(
+			$temp_file_name,
+			false
+		);
+
+        
+		$temp_file_extension = pathinfo(
+			$temp_file_name,
+			PATHINFO_EXTENSION
+		);
+
+		$temp_file_contents = file_get_contents(
+			$temp_file_name
+		);
+
+		$this->assertEquals(
+			$temp_file_extension,
+			$file_name_extension
+		);
+
+		$this->assertEquals(
+			$file_contents,
+			$temp_file_contents
+		);
+
+		unlink( $temp_file_name );
+	}
+}
