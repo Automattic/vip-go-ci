f40af0540a1eb90401693ffcc080062461a9e7d6
Gudmundur Haraldsson <gdh1@hi.is>
2019-02-12 13:58:28 +0000

    Test for adding pagebreaks

---
+++ tests/MiscMarkdownCommentAddPagebreakTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once( __DIR__ . '/../defines.php' );
+require_once( __DIR__ . '/../misc.php' );
+
+final class MiscMarkdownCommentAddPagebreakTest extends TestCase {
+	/**
+	 * @covers ::vipgoci_file_extension
+	 */
+	public function testPageBreak1() {
+		$mycomment = 'Here is my text. ' . "\n\r";
+
+		vipgoci_markdown_comment_add_pagebreak(
+			$mycomment,
+			'***'
+		);
+
+		$this->assertEquals(
+			'Here is my text. ' . "\n\r" . '***' . "\n\r",
+			$mycomment 
+		);
+	}
+}
