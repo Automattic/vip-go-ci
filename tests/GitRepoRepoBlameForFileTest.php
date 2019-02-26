c202f8caad340bc9d5f75776cf6aa569ebe6112b
Gudmundur Haraldsson <gdh1@hi.is>
2019-02-26 23:43:09 +0000

    Adding test for vipgoci_gitrepo_blame_for_file()

---
+++ tests/GitRepoRepoBlameForFileTest.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once( __DIR__ . '/IncludesForTests.php' );
+
+use PHPUnit\Framework\TestCase;
+
+final class GitRepoRepoBlameForFileTest extends TestCase {
+	var $options_git = array(
+		'git-path'			=> null,
+		'github-repo-url'		=> null,
+		'repo-owner'			=> null,
+		'repo-name'			=> null,
+		'github-token'			=> null,
+	);
+
+	var $options_git_repo_tests = array(
+		'commit-test-repo-blame-for-file-1'	=> null,
+	);
+
+	protected function setUp() {
+		vipgoci_unittests_get_config_values(
+			'git',
+			$this->options_git
+		);
+
+		vipgoci_unittests_get_config_values(
+			'git-repo-tests',
+			$this->options_git_repo_tests
+		);
+
+		$this->options = array_merge(
+			$this->options_git,
+			$this->options_git_repo_tests
+		);	
+	}
+
+	/**
+	 * @covers ::vipgoci_gitrepo_fetch_committed_file
+	 */
+	public function testRepoFetchTree1() {
+		$this->options['commit'] =
+			$this->options['commit-test-repo-blame-for-file-1'];
+
+		$this->options['local-git-repo'] =
+			vipgoci_unittests_setup_git_repo(
+				$this->options
+			);
+
+		ob_start();
+
+		if ( false === $this->options['local-git-repo'] ) {
+			$this->markTestSkipped(
+				'Could not set up git repository: ' .
+					ob_get_flush()
+			);
+		}
+
+		$this->options['token'] =
+			$this->options['github-token'];
+
+
+		$ret = vipgoci_gitrepo_blame_for_file(
+			$this->options['commit'],
+			'README.md',
+			$this->options['local-git-repo']
+		);
+
+		ob_end_clean();
+
+		$this->assertEquals(
+			json_encode( $ret ),
+			'[{"commit_id":"4869335189752462325aaef4838c9761d56195ce","file_name":"README.md","line_no":1},{"commit_id":"45b9e6479dfba4d54b584d53ace1814ce155d35e","file_name":"README.md","line_no":2}]'
+		);
+	}
+}
