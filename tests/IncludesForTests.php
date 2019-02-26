33665f7c44429e3d51250496c3c9f2df0bdf92a1
Gudmundur Haraldsson <gdh1@hi.is>
2019-02-26 17:30:38 +0000

    Adding function to initialize git repository in the tests.

--- tests/IncludesForTests.php
+++ tests/IncludesForTests.php
@@ -48,6 +48,94 @@ function vipgoci_unittests_get_config_values( $section, &$config_arr ) {
 	}
 }
 
-require_once( __DIR__ . '/../vip-go-ci.php' );
 
+/*
+ * Clone a git-repository and check out a
+ * particular revision.
+ */
+function vipgoci_unittests_setup_git_repo(
+	$options
+) {
+	$temp_dir = tempnam(
+		sys_get_temp_dir(),
+		'git-repo-dir-'
+	);
+
+	if ( false === $temp_dir ) {
+		return false;
+	}
+
+
+	$res = unlink( $temp_dir );
+
+	if ( false === $res ) {
+		return false;
+	}
+
+
+	$res = mkdir( $temp_dir );
+
+	if ( false === $res ) {
+		return false;
+	}
+
+
+	$cmd = sprintf(
+		'%s clone %s %s 2>&1',
+		escapeshellcmd( $options['git-path'] ),
+		escapeshellarg( $options['github-repo-url'] ),
+		escapeshellarg( $temp_dir )
+	);
+
+	$cmd_output = '';
+	$cmd_status = 0;
+
+	$res = exec( $cmd, $cmd_output, $cmd_status );
+
+	$cmd_output = implode( PHP_EOL, $cmd_output);
+
+	if (
+		( null === $cmd_output ) ||
+		( false !== strpos( $cmd_output, 'fatal' ) ) ||
+		( 0 !== $cmd_status )
+	) {
+		return false;
+	}
+
+	unset( $cmd );
+	unset( $cmd_output );
+	unset( $cmd_status );
+
+
+	$cmd = sprintf(
+		'%s -C %s checkout %s 2>&1',
+		escapeshellcmd( $options['git-path'] ),
+		escapeshellarg( $temp_dir ),
+		escapeshellarg( $options['commit'] )
+	);
+
+	$cmd_output = '';
+	$cmd_status = 0;
+
+	$res = exec( $cmd, $cmd_output, $cmd_status );
+
+	$cmd_output = implode( PHP_EOL, $cmd_output);
+
+	if (
+		( null === $cmd_output ) ||
+		( false !== strpos( $cmd_output, 'fatal:' ) ) ||
+		( 0 !== $cmd_status )
+	) {
+		return false;
+	}
+
+	unset( $cmd );
+	unset( $cmd_output );
+	unset( $cmd_status );
+
+
+	return $temp_dir;
+}
+
+require_once( __DIR__ . '/../vip-go-ci.php' );
 
