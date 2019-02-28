23b611092b8a815db7f28c81782aa711c915d0fb
Gudmundur Haraldsson <gdh1@hi.is>
2019-02-28 13:55:13 +0000

    Adding new test

--- tests/LintLintDoScanTest.php
+++ tests/LintLintDoScanTest.php
@@ -21,7 +21,7 @@ final class LintLintDoScanTest extends TestCase {
 	}
 
 	/**
-	 * @covers ::
+	 * @covers ::vipgoci_lint_do_scan
 	 */
 	public function testLintDoScan1() {
 		if ( null === $this->options_php['php-path'] ) {
@@ -52,6 +52,43 @@ final class LintLintDoScanTest extends TestCase {
 			$ret,
 			array(
 				'No syntax errors detected in ' . $php_file_path
+			)
+		);
+	}
+
+	/**
+	 * @covers ::vipgoci_lint_do_scan
+	 */
+	public function testLintDoScan2() {
+		if ( null === $this->options_php['php-path'] ) {
+			$this->markTestSkipped(
+				'Skipping test, not configured correctly'
+			);
+
+			return;
+		}
+
+		$php_file_path = vipgoci_save_temp_file(
+			'test-lint-do-scan-2',
+			'php',
+			'<?php ' . PHP_EOL . 'echo "foo"' . PHP_EOL
+		);
+
+		$php_file_name = pathinfo(
+			$php_file_path,
+			PATHINFO_FILENAME
+		);
+
+		$ret = vipgoci_lint_do_scan(
+			$this->options_php['php-path'],
+			$php_file_path
+		);
+
+		$this->assertEquals(
+			$ret,
+			array(
+				"PHP Parse error:  syntax error, unexpected end of file, expecting ',' or ';' in " . $php_file_path . " on line 3",
+				'Errors parsing ' . $php_file_path
 			)
 		);
 	}
