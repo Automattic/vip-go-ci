198939e216e0870a0bf884fdadc6d902063144c6
Gudmundur Haraldsson <gdh1@hi.is>
2019-03-06 13:26:26 +0000

    Fixing class name

--- tests/PhpcsScanScanCommitTest.php
+++ tests/PhpcsScanScanCommitTest.php
@@ -4,7 +4,7 @@ require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
-final class PhpcsScanDoScanTest extends TestCase {
+final class PhpcsScanScanCommitTest extends TestCase {
 	var $options_phpcs = array(
 		'phpcs-path'				=> null,
 		'phpcs-standard'			=> null,
