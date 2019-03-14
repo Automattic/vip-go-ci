1c0e0a7aa1e0db52b1d6ad9703375d52edcfa156
Gudmundur Haraldsson <gdh1@hi.is>
2019-03-14 18:32:53 +0000

    Adding a second test to make sure non-duplicates are not removed.

--- tests/PhpcsScanIssuesFilterDuplicate.php
+++ tests/PhpcsScanIssuesFilterDuplicate.php
@@ -62,4 +62,86 @@ final class PhpcsScanIssuesFilterDuplicate extends TestCase {
 			)
 		);
 	}
+
+	/**
+	 * @covers ::vipgoci_issues_filter_duplicate
+	 */
+	public function testFilterDuplicate2() {
+		$issues_filtered = vipgoci_issues_filter_duplicate(
+			array(
+				array(
+					'message' => 'json_encode() is discouraged. Use wp_json_encode() instead.',
+					'source' => 'WordPress.WP.AlternativeFunctions.json_encode_json_encode',
+					'severity' => 5,
+					'fixable' => false,
+					'type' => 'WARNING',
+					'line' => 7,
+					'column' => 6,
+					'level' => 'WARNING',
+				),
+
+				array(
+					'message' => 'json_encode() is discouraged. Use wp_json_encode() instead.',
+					'source' => 'WordPress.WP.AlternativeFunctions.json_encode_json_encode',
+					'severity' => 5,
+					'fixable' => false,
+					'type' => 'WARNING',
+					'line' => 7,
+					'column' => 6,
+					'level' => 'WARNING',
+				),
+
+				array(
+					'message' => 'json_encode() is discouraged. Use wp_json_encode() instead.',
+					'source' => 'WordPress.WP.AlternativeFunctions.json_encode_json_encode',
+					'severity' => 5,
+					'fixable' => false,
+					'type' => 'WARNING',
+					'line' => 7,
+					'column' => 6,
+					'level' => 'WARNING',
+				),
+
+				array(
+					'message' => 'json_encode() is discouraged. Use wp_json_encode() instead.',
+					'source' => 'WordPress.WP.AlternativeFunctions.json_encode_json_encode',
+					'severity' => 5,
+					'fixable' => false,
+					'type' => 'WARNING',
+					'line' => 80,
+					'column' => 6,
+					'level' => 'WARNING',
+				),
+
+
+			)
+		);
+
+		$this->assertEquals(
+			$issues_filtered,
+			array(
+				array(
+					'message' => 'json_encode() is discouraged. Use wp_json_encode() instead.',
+					'source' => 'WordPress.WP.AlternativeFunctions.json_encode_json_encode',
+					'severity' => 5,
+					'fixable' => false,
+					'type' => 'WARNING',
+					'line' => 7,
+					'column' => 6,
+					'level' => 'WARNING',
+				),
+
+				array(
+					'message' => 'json_encode() is discouraged. Use wp_json_encode() instead.',
+					'source' => 'WordPress.WP.AlternativeFunctions.json_encode_json_encode',
+					'severity' => 5,
+					'fixable' => false,
+					'type' => 'WARNING',
+					'line' => 80,
+					'column' => 6,
+					'level' => 'WARNING',
+				),
+			)
+		);
+	}
 }
