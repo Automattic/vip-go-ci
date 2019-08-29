24a07006d1b3018b4b54bd94b9842781786ae4c4
Gudmundur Haraldsson <gdh1@hi.is>
2019-08-29 10:17:47 +0000

    Updating comment.

--- misc.php
+++ misc.php
@@ -1590,7 +1590,7 @@ function vipgoci_markdown_comment_add_pagebreak(
 
 /*
  * Sanitize a string, removing any whitespace-characters
- * from the beginning and and, and transform to lowercase.
+ * from the beginning and end, and transform to lowercase.
  */
 function vipgoci_sanitize_string( $str ) {
 	return strtolower( ltrim( rtrim(
