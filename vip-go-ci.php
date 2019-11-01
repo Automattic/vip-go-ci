a22b59a59f149074e5515771b2db87418cc3c757
Gudmundur Haraldsson <gdh1@hi.is>
2019-11-01 14:35:37 +0000

    Implement cache-clearing for vipgoci_cache().

--- vip-go-ci.php
+++ vip-go-ci.php
@@ -419,6 +419,15 @@ function vipgoci_run() {
 	global $argv;
 	global $vipgoci_debug_level;
 
+	/*
+	 * Clear the internal
+	 * cache before doing anything.
+	 */
+	vipgoci_cache(
+		VIPGOCI_CACHE_CLEAR,
+		null
+	);
+
 	$hashes_oauth_arguments =
 		array(
 			'hashes-oauth-token',
