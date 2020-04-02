7e50fe2ff15cfdce6bc7ecf89708f1c64f37ac85
Gudmundur Haraldsson <gdh1@hi.is>
2020-04-02 16:29:14 +0000

    Update help message.

--- vip-go-ci.php
+++ vip-go-ci.php
@@ -296,8 +296,9 @@ function vipgoci_run() {
 			"\t" . '--phpcs-skip-folders=STRING    Specify folders relative to root of the git repository in which ' . PHP_EOL .
 			"\t" . '                               files are not to be scanned using PHPCS. Values are comma' . PHP_EOL .
 			"\t" . '                               separated' . PHP_EOL .
-			"\t" . '--phpcs-skip-folders-in-repo-options-file=BOOL   Allows folders that are not to be PHPCS scanned to ' . PHP_EOL .
-			"\t" . '                                                 be specified in options file in repository. ' . PHP_EOL .
+			"\t" . '--phpcs-skip-folders-in-repo-options-file=BOOL   Allows folders that are not to be PHPCS ' . PHP_EOL .
+			"\t" . '                                                 scanned to be specified in options file ' . PHP_EOL .
+			"\t" . '                                                 in repository (.vipgoci_phpcs_skip_folders).' . PHP_EOL .
 			PHP_EOL .
 			"\t" . '--autoapprove=BOOL             Whether to auto-approve Pull-Requests' . PHP_EOL .
 			"\t" . '                               altering only files of certain types or ' . PHP_EOL .
@@ -349,7 +350,9 @@ function vipgoci_run() {
 			"\t" . '--lint=BOOL                    Whether to do PHP linting (true/false)' . PHP_EOL .
 			"\t" . '--lint-skip-folders=STRING     Specify folders relative to root of the git repository in which ' . PHP_EOL .
 			"\t" . '                               files should not be PHP linted. Values are comma separated.' . PHP_EOL .
-			"\t" . '--lint-skip-folders-in-repo-options-file=BOOL  ' . PHP_EOL .
+			"\t" . '--lint-skip-folders-in-repo-options-file=BOOL   Allows folders that are not to be PHP Linted ' . PHP_EOL .
+			"\t" . '                                                to be specified in options file in repository ' . PHP_EOL .
+			"\t" . '                                                (.vipgoci_lint_skip_folders).' . PHP_EOL .
 			PHP_EOL .
 			"\t" . '--help                         Displays this message' . PHP_EOL .
 			"\t" . '--env-options=STRING           Specifies configuration options to be read from environmental ' . PHP_EOL .
