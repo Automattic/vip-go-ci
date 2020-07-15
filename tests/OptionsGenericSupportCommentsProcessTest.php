3af5459c5109d74432d47e9db20574a0d7166b48
Gudmundur Haraldsson <gdh1@hi.is>
2020-07-15 13:53:25 +0000

    Add tests for lower and non-lower case

--- tests/OptionsGenericSupportCommentsProcessTest.php
+++ tests/OptionsGenericSupportCommentsProcessTest.php
@@ -18,7 +18,7 @@ final class OptionsGenericSupportCommentsProcess extends TestCase {
 	 */
 	public function testOptionGenericSupportCommentProcessBoolean() {
 		$this->options['myoption1'] =
-			'1:false|||5:true|||10:false|||15:true';
+			'1:false|||5:true|||10:false|||15:trUE';
 
 		vipgoci_option_generic_support_comments_process(
 			$this->options,
@@ -40,14 +40,15 @@ final class OptionsGenericSupportCommentsProcess extends TestCase {
 	/**
 	 * @covers ::vipgoci_option_generic_support_comments_process
 	 */
-	public function testOptionGenericSupportCommentProcessStringString() {
+	public function testOptionGenericSupportCommentProcessStringStringNotLower() {
 		$this->options['myoption2'] =
-			'3:bar|||6:foo|||9:bar|||12:foo|||15:false';
+			'3:bar|||6:foo|||9:bar|||12:foo|||15:false|||20:AbCdEfG';
 
 		vipgoci_option_generic_support_comments_process(
 			$this->options,
 			'myoption2',
-			'string'
+			'string',
+			false
 		);
 
 		$this->assertEquals(
@@ -57,6 +58,7 @@ final class OptionsGenericSupportCommentsProcess extends TestCase {
 				9	=> 'bar',
 				12	=> 'foo',
 				15	=> 'false',
+				20	=> 'AbCdEfG',
 			),
 			$this->options['myoption2']
 		);
@@ -65,14 +67,42 @@ final class OptionsGenericSupportCommentsProcess extends TestCase {
 	/**
 	 * @covers ::vipgoci_option_generic_support_comments_process
 	 */
-	public function testOptionGenericSupportCommentProcessArray() {
+	public function testOptionGenericSupportCommentProcessStringStringLower() {
+		$this->options['myoption2'] =
+			'3:bar|||6:foo|||9:bar|||12:foo|||15:false|||20:AbCdEfG';
+
+		vipgoci_option_generic_support_comments_process(
+			$this->options,
+			'myoption2',
+			'string',
+			true
+		);
+
+		$this->assertEquals(
+			array(
+				3	=> 'bar',
+				6	=> 'foo',
+				9	=> 'bar',
+				12	=> 'foo',
+				15	=> 'false',
+				20	=> 'abcdefg',
+			),
+			$this->options['myoption2']
+		);
+	}
+
+	/**
+	 * @covers ::vipgoci_option_generic_support_comments_process
+	 */
+	public function testOptionGenericSupportCommentProcessArrayNotLower() {
 		$this->options['myoption3'] =
-			'3:foo,bar,test|||6:test,foo,foo|||9:aaa,bbb,ccc|||12:ddd|||15:';
+			'3:foo,bar,test|||6:test,foo,foo|||9:aaa,bbb,ccc|||12:ddd|||15:|||20:AbCdEfG';
 
 		vipgoci_option_generic_support_comments_process(
 			$this->options,
 			'myoption3',
-			'array'
+			'array',
+			false
 		);
 
 		$this->assertEquals(
@@ -89,7 +119,48 @@ final class OptionsGenericSupportCommentsProcess extends TestCase {
 				12	=> array(
 					'ddd',
 				),
-				15	=> array(				
+				15	=> array(
+				),
+				20	=> array(
+					'AbCdEfG',
+				)
+			),
+			$this->options['myoption3']
+		);
+	}
+
+	/**
+	 * @covers ::vipgoci_option_generic_support_comments_process
+	 */
+	public function testOptionGenericSupportCommentProcessArrayLower() {
+		$this->options['myoption3'] =
+			'3:foo,bar,test|||6:test,foo,foo|||9:aaa,bbb,ccc|||12:ddd|||15:|||20:AbCdEfG';
+
+		vipgoci_option_generic_support_comments_process(
+			$this->options,
+			'myoption3',
+			'array',
+			true
+		);
+
+		$this->assertEquals(
+			array(
+				3	=> array(
+					'foo', 'bar', 'test'
+				),
+				6	=> array(
+					'test', 'foo', 'foo',
+				),
+				9	=> array(
+					'aaa', 'bbb', 'ccc',
+				),
+				12	=> array(
+					'ddd',
+				),
+				15	=> array(
+				),
+				20	=> array(
+					'abcdefg',
 				)
 			),
 			$this->options['myoption3']
