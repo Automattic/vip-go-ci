aec624f0d7157da5f2ba9020f5669bd0c9a5a46a
Gudmundur Haraldsson <gdh1@hi.is>
2020-08-03 13:58:42 +0000

    Add comment

--- vip-go-ci.php
+++ vip-go-ci.php
@@ -1,5 +1,12 @@
 #!/usr/bin/php
 <?php
 
+/*
+ * Require main.php which does the rest.
+ *
+ * Including is done so that the interpreter
+ * line above is not printed when vip-go-ci 
+ * is included as a library.
+ */
 require_once( __DIR__ . '/main.php' );
 
