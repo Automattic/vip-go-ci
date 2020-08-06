7f2e6a0b6dd48bc62d7aa3e259b763460147b8f3
Gudmundur Haraldsson <gdh1@hi.is>
2020-08-06 16:18:29 +0000

    Update comment for clarity.

--- vip-go-ci.php
+++ vip-go-ci.php
@@ -4,9 +4,9 @@
 /*
  * Require main.php which does the rest.
  *
- * Including is done so that the interpreter
+ * This is done so that the interpreter
  * line above is not printed when vip-go-ci 
- * is included as a library.
+ * is used as a library.
  */
 require_once( __DIR__ . '/main.php' );
 
