4af2fcaf05518566f13ad5ee54a81e75c0ce99cc
Gudmundur Haraldsson <gdh1@hi.is>
2021-04-08 16:15:37 +0000

    Add test for vipgoci_sanitize_path_prefix()

---
+++ tests/MiscSanitizePathPrefix.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once( __DIR__ . '/IncludesForTests.php' );
+
+use PHPUnit\Framework\TestCase;
+
+final class MiscSanitizePathPrefix extends TestCase {
+	/**
+	 * @covers ::vipgoci_sanitize_path_prefix
+	 */
+	public function testSanitizePathPrefix1() {
+		$path = vipgoci_sanitize_path_prefix(
+			'a/folder1',
+			'a/'
+		);
+
+		$this->assertSame(
+			'folder1',
+			$path
+		);
+	}
+
+	public function testSanitizePathPrefix2() {
+		$path = vipgoci_sanitize_path_prefix(
+			'a/folder1',
+			'b/'
+		);
+
+		$this->assertSame(
+			'a/folder1',
+			$path
+		);
+	}
+}
