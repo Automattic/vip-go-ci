df816437ea240ee4e31d4bca484d80924af6c205
Gudmundur Haraldsson <gdh1@hi.is>
2021-04-09 18:02:52 +0000

    Update unit-test so to use vip-go-ci-testing repository

--- tests/MiscPatchChangedLinesTest.php
+++ tests/MiscPatchChangedLinesTest.php
@@ -7,14 +7,12 @@ use PHPUnit\Framework\TestCase;
 final class MiscPatchChangedLinesTest extends TestCase {
 	var $options_git = array(
 		'git-path'		=> null,
+		'github-repo-url'	=> null,
 	);
 
-	var $options_github = array(
-		'repo-owner'	=> null,
-		'repo-name'	=> null,
-		'pr-base-sha'	=> null,
-		'commit-id'	=> null,
-		'github-repo-url'	=> null, // FIXME: Should use vip-go-ci-tests repo
+	var $options_patch_changed_lines = array(
+		'pr-base-sha'		=> null,
+		'commit-id'		=> null,
 	);
 
 	protected function setUp(): void {
@@ -25,12 +23,12 @@ final class MiscPatchChangedLinesTest extends TestCase {
 
 		vipgoci_unittests_get_config_values(
 			'patch-changed-lines',
-			$this->options_github
+			$this->options_patch_changed_lines
 		);
 
 		$this->options = array_merge(
 			$this->options_git,
-			$this->options_github
+			$this->options_patch_changed_lines
 		);
 
 		$this->options[ 'github-token' ] =
@@ -85,14 +83,14 @@ final class MiscPatchChangedLinesTest extends TestCase {
 			$this->options['local-git-repo'],
 			$this->options['pr-base-sha'],
 			$this->options['commit-id'],
-			'ap-hashes-api.php'
+			'README.md'
 		);
 
 		vipgoci_unittests_output_unsuppress();
 
 		$this->assertEquals(
 			json_decode(
-				'[null,"194",195,196,null,197,198,199,200,201,202]',
+				'[null,1,null,"1",2,3,4,5,6,7]',
 				true
 			),
 			$patch_arr
