cdc2c891d8e1179cde9584c99c4ee528e9f14ebe
Gudmundur Haraldsson <gdh1@hi.is>
2021-04-13 11:42:42 +0000

    Adding new, multicomponent test

--- tests/GitHubDiffsFetchUncachedTest.php
+++ tests/GitHubDiffsFetchUncachedTest.php
@@ -13,6 +13,8 @@ final class GitHubDiffsFetchUncachedTest extends TestCase {
 		'commit-test-repo-pr-diffs-1-e'	=> null,
 		'commit-test-repo-pr-diffs-1-f' => null,
 		'commit-test-repo-pr-diffs-1-g' => null,
+		'commit-test-repo-pr-diffs-2-a' => null,
+		'commit-test-repo-pr-diffs-2-b' => null,
 	);
 
 	var $options_git = array(
@@ -378,6 +380,144 @@ final class GitHubDiffsFetchUncachedTest extends TestCase {
 						'additions'	=> 0,
 						'deletions'	=> 1,
 						'changes'	=> 1,
+					),
+				),
+			),
+			$diff
+		);
+	}
+
+	/**
+	 * Check diff between commits.
+	 *
+	 * @covers ::vipgoci_gitrepo_diffs_fetch_uncached
+	 */
+	public function testGitHubDiffsFetch6() {
+		$options_test = vipgoci_unittests_options_test(
+			$this->options,
+			array( 'github-token', 'token' ),
+			$this
+		);
+
+		if ( -1 === $options_test ) {
+			return;
+		}
+
+		vipgoci_unittests_output_suppress();
+
+		$this->options['local-git-repo'] =
+			vipgoci_unittests_setup_git_repo(
+				$this->options
+			);
+
+		if ( false === $this->options['local-git-repo'] ) {
+			$this->markTestSkipped(
+				'Could not set up git repository: ' .
+					vipgoci_unittests_output_get()
+			);
+
+			return;
+		}
+
+		$diff = vipgoci_gitrepo_diffs_fetch_uncached(
+			$this->options['local-git-repo'],
+			$this->options['commit-test-repo-pr-diffs-2-a'],
+			$this->options['commit-test-repo-pr-diffs-2-b']
+		);
+
+		vipgoci_unittests_output_unsuppress();
+
+		$this->assertSame(
+			array(
+				'files' => array(
+					'a/new-file.txt'		=> array(
+						/* File added, starting with name 'a/' */
+						'filename'	=> 'a/new-file.txt',
+						'patch'		=> '@@ -0,0 +1,2 @@' . PHP_EOL . '+Line 1' . PHP_EOL . '+Line 2',
+						'status'	=> 'added',
+						'additions'	=> 2,
+						'deletions'	=> 0,
+						'changes'	=> 2,
+					),
+
+					'b/new-file.txt'		=> array(
+						/* File added, starting with name 'b/' */
+						'filename'	=> 'b/new-file.txt',
+						'patch'		=> '@@ -0,0 +1,3 @@' . PHP_EOL . '+Test 1' . PHP_EOL . '+Test 2' . PHP_EOL . '+Test 3',
+						'status'	=> 'added',
+						'additions'	=> 3,
+						'deletions'	=> 0,
+						'changes'	=> 3,
+					),
+
+					'new-file-permissions-changed.txt'	=> array(
+						/* New file, with permissions changed */
+						'filename'	=> 'new-file-permissions-changed.txt',
+						'patch'		=> '@@ -0,0 +1,4 @@' . PHP_EOL . '+This is also a new file.' . PHP_EOL . '+Line 2' . PHP_EOL . '+Line 3' . PHP_EOL . '+Line 4',
+						'status'	=> 'added',
+						'additions'	=> 4,
+						'deletions'	=> 0,
+						'changes'	=> 4,
+					),
+
+					'new-file.txt'			=> array(
+						/* New file */
+						'filename'	=> 'new-file.txt',
+						'patch'		=> '@@ -0,0 +1,4 @@' . PHP_EOL . '+This is a new file.' . PHP_EOL . '+Line 2' . PHP_EOL . '+Line 3' . PHP_EOL . '+Line 4',
+						'status'	=> 'added',
+						'additions'	=> 4,
+						'deletions'	=> 0,
+						'changes'	=> 4,
+					),
+
+					'permission-changed.txt'	=> array(
+						/* Permission changed */
+						'filename'	=> 'permission-changed.txt',
+						'patch'		=> '',
+						'status'	=> 'modified',
+						'additions'	=> 0,
+						'deletions'	=> 0,
+						'changes'	=> 0,
+					),
+
+					'to-be-added-to.txt'		=> array(
+						/* File was added to */
+						'filename'	=> 'to-be-added-to.txt',
+						'patch'		=> '@@ -1,2 +1,4 @@' . PHP_EOL . ' File to be added to.' . PHP_EOL . ' Line 2' . PHP_EOL . '+Line 3' . PHP_EOL . '+Line 4',
+						'status'	=> 'modified',
+						'additions'	=> 2,
+						'deletions'	=> 0,
+						'changes'	=> 2,
+					),
+
+					'to-be-removed-from.txt'	=> array(
+						/* File to be removed from */
+						'filename'	=> 'to-be-removed-from.txt',
+						'patch'		=> '@@ -1,3 +1 @@' . PHP_EOL . ' Content of file' . PHP_EOL . '-Line 2' . PHP_EOL . '-Line 3',
+						'status'	=> 'modified',
+						'additions'	=> 0,
+						'deletions'	=> 2,
+						'changes'	=> 2,
+					),
+
+					'to-be-renamed.txt'		=> array(
+						/* File was actually renamed, but git also indicates removal */
+						'filename'	=> 'to-be-renamed.txt',
+						'patch'		=> '@@ -1,4 +0,0 @@' . PHP_EOL . '-This file will be renamed.' . PHP_EOL . '-Line 2' . PHP_EOL . '-Line 3' . PHP_EOL . '-Line 4',
+						'status'	=> 'removed',
+						'additions'	=> 0,
+						'deletions'	=> 4,
+						'changes'	=> 4,
+					),
+
+					'to-be-renamed2.txt'		=> array(
+						/* File was renamed */
+						'filename'	=> 'to-be-renamed2.txt',
+						'patch'		=> '',
+						'status'	=> 'renamed',
+						'additions'	=> 0,
+						'deletions'	=> 0,
+						'changes'	=> 0,
 					),
 				),
 			),
