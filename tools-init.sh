cd1dad49852093781708ff9ab4a49cffbacb4551
Gudmundur Haraldsson <gdh1@hi.is>
2021-04-30 08:59:13 +0000

    Update to version 2.3.2

--- tools-init.sh
+++ tools-init.sh
@@ -15,8 +15,8 @@ export WP_CODING_STANDARDS_VER="2.3.0"
 export WP_CODING_STANDARDS_SHA1SUM="c8161d77fcf63bdeaa3e8e6aa36bc1936b469070";
 
 # https://github.com/automattic/vip-coding-standards/releases
-export VIP_CODING_STANDARDS_VER="2.3.1"
-export VIP_CODING_STANDARDS_SHA1SUM="e453ebe79917af242de572edccffb2933fc83364";
+export VIP_CODING_STANDARDS_VER="2.3.2"
+export VIP_CODING_STANDARDS_SHA1SUM="fd3833fa69feb8ab3f02c894c00790ea45e7103d";
 
 # https://github.com/sirbrillig/phpcs-variable-analysis/releases
 export PHPCS_VARIABLE_ANALYSIS_VER="2.9.0"
