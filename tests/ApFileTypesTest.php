94af148d48cf83bd9d7de105dc9bf86a0f7bd4ca
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-04 16:55:57 +0000

    Fix test when no access token is available (hopefully)

--- tests/ApFileTypesTest.php
+++ tests/ApFileTypesTest.php
@@ -40,6 +40,10 @@ final class ApFileTypesTest extends TestCase {
 				true // Fetch from secrets file
 			);
 
+		if ( empty( $this->options[ 'github-token' ] ) ) {
+			$this->options[ 'github-token' ] = '';
+		}
+
 		$this->options['token'] =
 			$this->options['github-token'];
 
