e50dd6dc02a75872fdabb999278a635ee74cccfe
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-04 16:40:49 +0000

    Renaming file to match function.

--- tests/GitRepoDiffsFetchUnfilteredTest.php
+++ tests/GitRepoDiffsFetchUnfilteredTest.php
@@ -4,7 +4,7 @@ require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
-final class GitRepoDiffsFetchUncachedTest extends TestCase {
+final class GitRepoDiffsFetchUnfilteredTest extends TestCase {
 	var $options_git_repo_tests = array(
 		'commit-test-repo-pr-diffs-1-a'	=> null,
 		'commit-test-repo-pr-diffs-1-b'	=> null,
