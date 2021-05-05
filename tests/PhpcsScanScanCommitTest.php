8a5aabce79f57ab166ceee1008acb5050956bc05
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-05 14:02:47 +0000

    Define github-token, if not defined in INI

--- tests/PhpcsScanScanCommitTest.php
+++ tests/PhpcsScanScanCommitTest.php
@@ -47,6 +47,10 @@ final class PhpcsScanScanCommitTest extends TestCase {
 				true // Fetch from secrets file
 			);
 
+		if ( empty( $this->options['github-token'] ) ) {
+			$this->options['github-token'] = ''; 
+		}
+
 		$this->options['token'] =
 			$this->options['github-token'];
 
