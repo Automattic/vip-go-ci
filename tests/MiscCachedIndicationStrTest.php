38f6ce8b1b4b6aa7a76641126ecfbab9763aeba2
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-05 15:32:50 +0000

    Disable a certain PHPCS sniff

--- tests/MiscCachedIndicationStrTest.php
+++ tests/MiscCachedIndicationStrTest.php
@@ -6,6 +6,8 @@ require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+// phpcs:disable PSR1.Files.SideEffects
+
 final class MiscCachedIndicationStrTest extends TestCase {
 	/**
 	 * @covers ::vipgoci_cached_indication_str
