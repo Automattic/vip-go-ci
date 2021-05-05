8c5ef90cf138c4cd1e8af222b148e2e0f7873cb7
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-05 15:28:07 +0000

    Adding unit-test for vipgoci_cached_indication_str()

---
+++ tests/MiscCachedIndicationStrTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Vipgoci\tests;
+
+require_once( __DIR__ . '/IncludesForTests.php' );
+
+use PHPUnit\Framework\TestCase;
+
+final class MiscCachedIndicationStrTest extends TestCase {
+	/**
+	 * @covers ::vipgoci_cached_indication_str
+	 */
+	public function testCachedIndicationStr1() {
+		$this->assertsame(
+			' (cached)',
+			vipgoci_cached_indication_str(
+				true
+			)
+		);
+
+		$this->assertsame(
+			' (cached)',
+			vipgoci_cached_indication_str(
+				array( 1, 2, 3 ),
+			)
+		);
+
+		$this->assertsame(
+			'',
+			vipgoci_cached_indication_str(
+				false,
+			)
+		);
+	}
+}
