87eab1d3bb9cc63c7aa5015a7a00d9eb3646e6f1
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-07 12:24:28 +0000

    Replace assertEquals() with assertSame(), along with adjustments

--- tests/MiscTempTest.php
+++ tests/MiscTempTest.php
@@ -34,12 +34,12 @@ final class MiscTempTest extends TestCase {
 			$temp_file_name
 		);
 
-		$this->assertEquals(
-			$file_name_extension,
+		$this->assertSame(
+			'',
 			$temp_file_extension
 		);
 
-		$this->assertEquals(
+		$this->assertSame(
 			$file_contents,
 			$temp_file_contents
 		);
@@ -52,7 +52,7 @@ final class MiscTempTest extends TestCase {
 	 */
 	public function testTempFile2() {
 		$file_name_prefix = 'myfilename2';
-		$file_name_extension = 'txt';
+		$file_name_extension = '';
 		$file_contents = 'mycontentsofthefile2' . PHP_EOL;
 
 		$temp_file_name = vipgoci_save_temp_file(
@@ -76,12 +76,55 @@ final class MiscTempTest extends TestCase {
 			$temp_file_name
 		);
 
-		$this->assertEquals(
+		$this->assertSame(
 			$file_name_extension,
 			$temp_file_extension
 		);
 
-		$this->assertEquals(
+		$this->assertSame(
+			$file_contents,
+			$temp_file_contents
+		);
+
+		unlink( $temp_file_name );
+	}
+
+
+	/**
+	 * @covers ::vipgoci_save_temp_file
+	 */
+	public function testTempFile3() {
+		$file_name_prefix = 'myfilename3';
+		$file_name_extension = 'txt';
+		$file_contents = 'mycontentsofthefile3' . PHP_EOL;
+
+		$temp_file_name = vipgoci_save_temp_file(
+			$file_name_prefix,
+			$file_name_extension,
+			$file_contents
+		);
+
+		$this->assertNotEquals(
+			false,
+			$temp_file_name
+		);
+
+		
+		$temp_file_extension = pathinfo(
+			$temp_file_name,
+			PATHINFO_EXTENSION
+		);
+
+		$temp_file_contents = file_get_contents(
+			$temp_file_name
+		);
+
+		$this->assertSame(
+			$file_name_extension,
+			$temp_file_extension
+		);
+
+		$this->assertSame(
 			$file_contents,
 			$temp_file_contents
 		);
