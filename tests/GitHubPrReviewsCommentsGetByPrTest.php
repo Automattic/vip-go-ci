ee882ff2f0cdfb9c680927762493d89c93412b6f
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-07 13:00:49 +0000

    Improved tests

--- tests/GitHubPrReviewsCommentsGetByPrTest.php
+++ tests/GitHubPrReviewsCommentsGetByPrTest.php
@@ -97,4 +97,89 @@ final class GitHubPrReviewsCommentsGetByPrTest extends TestCase {
 			$comments_actual[0]->body
 		);
 	}
+
+	/**
+	 * @covers ::vipgoci_github_pr_reviews_comments_get_by_pr
+	 */
+	public function testGitHubPrReviewsCommentsGetByPr2() {
+		$options_test = vipgoci_unittests_options_test(
+			$this->options,
+			array( 'github-token', 'token' ),
+			$this
+		);
+
+		if ( -1 === $options_test ) {
+			return;
+		}
+
+		vipgoci_unittests_output_suppress();
+
+		$comments_actual = vipgoci_github_pr_reviews_comments_get_by_pr(
+			$this->options,
+			$this->options['pr-test-github-pr-reviews-get-1'],
+			array(
+				'login'	=> 'gudmdharalds',
+			)
+		);
+
+		vipgoci_unittests_output_unsuppress();
+
+		$this->assertSame(
+			1,
+			count( $comments_actual )
+		);
+
+		$this->assertSame(
+			264037556,
+			$comments_actual[0]->id
+		);
+
+
+		$this->assertSame(
+			'file1.php',
+			$comments_actual[0]->path
+		);
+
+		$this->assertSame(
+			3,
+			$comments_actual[0]->position
+		);
+
+		$this->assertSame(
+			'All output should be escaped.',
+			$comments_actual[0]->body
+		);
+	}
+
+	/**
+	 * @covers ::vipgoci_github_pr_reviews_comments_get_by_pr
+	 */
+	public function testGitHubPrReviewsCommentsGetByPr3() {
+		$options_test = vipgoci_unittests_options_test(
+			$this->options,
+			array( 'github-token', 'token' ),
+			$this
+		);
+
+		if ( -1 === $options_test ) {
+			return;
+		}
+
+		vipgoci_unittests_output_suppress();
+
+		$comments_actual = vipgoci_github_pr_reviews_comments_get_by_pr(
+			$this->options,
+			$this->options['pr-test-github-pr-reviews-get-1'],
+			array(
+				'login'	=> 'random_invalid_user___0x0',
+			)
+		);
+
+		vipgoci_unittests_output_unsuppress();
+
+		$this->assertSame(
+			0,
+			count( $comments_actual )
+		);
+	}
 }
