78c6d971075df2be1e70e664ccdc4eddc670a7ec
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-11 14:38:42 +0000

    Adding code to parse options and validate input

--- github-commit-status.php
+++ github-commit-status.php
@@ -1,35 +1,77 @@
 #!/usr/bin/env php
 <?php
 
+// phpcs:disable PSR1.Files.SideEffects
+
 define( 'VIPGOCI_INCLUDED', true );
 
 require_once( __DIR__ . '/requires.php' );
 
-global $argv;
+vipgoci_log(
+	'Preparing to set GitHub build status'
+);
 
 /*
  * Set how to deal with errors:
  * Report all errors, and display them.
  */
 ini_set( 'error_log', '' );
-        
+	
 error_reporting( E_ALL );
 ini_set( 'display_errors', 'on' );
 
-        
+/*
+ * Recognized options, get options
+ */
+$options_recognized = array(
+	'env-options:',
+	'repo-owner:',
+	'repo-name:',
+	'github-token:',
+	'github-commit:',
+	'build-state:',
+	'build-description:',
+	'build-context:',
+);
+
 $options = getopt(
 	null,
+	$options_recognized
+);
+
+/*
+ * Options to remove of any sensitive
+ * detail when cleaned later.
+ */
+vipgoci_options_sensitive_clean(
+	null,
 	array(
-		'repo-owner:',
-		'repo-name:',
-		'github-token:',
-		'github-commit:',
-		'build-state:',
-		'build-description:',
-		'build-context:',
+		'github-token'
 	)
 );
 
+
+/*
+ * Parse options
+ */
+vipgoci_option_array_handle(
+	$options,
+	'env-options',
+	array(),
+	null,
+	',',
+	false
+);
+
+vipgoci_options_read_env(
+	$options,
+	$options_recognized
+);
+
+/*
+ * Verify we have all the options
+ * we need.
+ */
 if ( ! isset(
 	$options['repo-owner'],
 	$options['repo-name'],
@@ -44,6 +86,38 @@ if ( ! isset(
 	);
 }
 
+/*
+ * Verify that --build-state is of valid
+ * value.
+ */
+switch( $options['build-state'] ) {
+	case 'pending':
+	case 'failure':
+	case 'success':
+		break;
+
+	default:
+		vipgoci_sysexit(
+			'Invalid parameter for --build-state, only "pending", "failure", and "success" are valid',
+			array(
+				'build-state'	=> $options['build-state'],
+			)
+		);
+}
+
+/*
+ * Log that we are setting build
+ * status and set it.
+ */
+vipgoci_log(
+	'Setting build status for commit ...',
+	array(
+		'options' => vipgoci_options_sensitive_clean(
+			$options
+		)
+	)
+);
+
 vipgoci_github_status_create(
 	$options['repo-owner'],
 	$options['repo-name'],
@@ -55,3 +129,6 @@ vipgoci_github_status_create(
 	$options['build-context']
 );
 
+vipgoci_log(
+	'Finished, exiting',
+);
