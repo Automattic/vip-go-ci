7e8f88801e96049aa64e35cedff37b258734b695
Gudmundur Haraldsson <gdh1@hi.is>
2021-05-13 13:09:45 +0000

    Add --help

--- github-commit-status.php
+++ github-commit-status.php
@@ -7,9 +7,6 @@ define( 'VIPGOCI_INCLUDED', true );
 
 require_once( __DIR__ . '/requires.php' );
 
-vipgoci_log(
-	'Preparing to set GitHub build status'
-);
 
 /*
  * Configure PHP error reporting.
@@ -68,18 +65,40 @@ vipgoci_options_read_env(
  * Verify we have all the options
  * we need.
  */
-if ( ! isset(
-	$options['repo-owner'],
-	$options['repo-name'],
-	$options['github-token'],
-	$options['github-commit'],
-	$options['build-state'],
-	$options['build-description'],
-	$options['build-context']
-) ) {
-	vipgoci_sysexit(
-		'Missing parameter'
-	);
+if (
+	( ! isset(
+		$options['repo-owner'],
+		$options['repo-name'],
+		$options['github-token'],
+		$options['github-commit'],
+		$options['build-state'],
+		$options['build-description'],
+		$options['build-context']
+	) )
+	||
+	(
+		isset( $options['help'] )
+	)
+) {
+	print 'Usage: ' . $argv[0] . PHP_EOL .
+		PHP_EOL .
+		"\t" . '--repo-owner=STRING            Specify repository owner, can be an organization' . PHP_EOL .
+		"\t" . '--repo-name=STRING             Specify name of the repository' . PHP_EOL .
+		"\t" . '--github-token=STRING          The access-token to use to communicate with GitHub' . PHP_EOL .
+		"\t" . '--github-commit=STRING         Specify the exact commit to set state for' . PHP_EOL .
+		PHP_EOL .
+		"\t" . '--build-state=STRING           Specify build state, one of: "pending", "failure", ' . PHP_EOL .
+		"\t" . '                               and "success" are valid.' . PHP_EOL .
+		"\t" . '--build-description=STRING     Specify description for end-user, displayed along with ' . PHP_EOL .
+		"\t" . '                               state.' . PHP_EOL .
+		"\t" . '--build-context=STRING         Specify context, a consistent identifier used across ' . PHP_EOL .
+		"\t" . '                               all the build states' . PHP_EOL .
+		PHP_EOL .
+		"\t" . '--help                         Prints this message.' . PHP_EOL .
+		PHP_EOL .
+		'All options are mandatory.' . PHP_EOL;
+
+	exit( VIPGOCI_EXIT_USAGE_ERROR );
 }
 
 /*
