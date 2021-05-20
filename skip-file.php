e18bdb64f83daebcad3101a1d0dd22da61b4d9a2
Ariana Kataoka <aris.kataoka@gmail.com>
2021-05-20 22:14:44 +1000

    Add skip-file logic into a separated file

---
+++ skip-file.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Logic related to skip files
+ */
+/**
+ * @param array $skipped
+ * @param array $validation
+ * @todo move this to the main/skipped logic?
+ * @return array
+ */
+function vipgoci_get_skipped_files( $skipped, $validation ) {
+	$skipped[ 'issues' ] = array_merge_recursive( $skipped['issues'], $validation[ 'issues' ] );
+	$skipped[ 'total' ] += $validation[ 'total' ];
+
+	return $skipped;
+}
+
+/**
+ * @param array $commit_skipped_files
+ * @param array $validation
+ * @param int $pr_number
+ */
+function vipgoci_set_skipped_file( &$commit_skipped_files, $validation, $pr_number )
+{
+	$commit_skipped_files[ $pr_number ] = vipgoci_get_skipped_files(
+		$commit_skipped_files[ $pr_number ],
+		$validation
+	);
+}
+
+/**
+ * @param array $prs_implicated
+ * @param array $commit_skipped_files
+ * @param array $validation
+ */
+function vipgoci_set_prs_implicated_skipped_files( $prs_implicated, &$commit_skipped_files, $validation )
+{
+	foreach ( $prs_implicated as $pr_item ) {
+		vipgoci_set_skipped_file( $commit_skipped_files, $validation, $pr_item->number );
+	}
+}
