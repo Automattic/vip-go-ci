dea9e1a9357d29b0423165fe0ebe17105928c2e4
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2021-09-20 10:38:16 +0000

    Unit test for vipgoci_options_recognized()

---
+++ tests/VipgociOptionsRecognizedTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Vipgoci\tests;
+
+require_once( __DIR__ . '/IncludesForTests.php' );
+
+use PHPUnit\Framework\TestCase;
+
+// phpcs:disable PSR1.Files.SideEffects
+
+final class VipgociOptionsRecognizedTest extends TestCase {
+	/**
+	 * @covers ::vipgoci_options_recognized
+	 */
+	public function testOptionsRecognized() {
+		$options_recognized_arr = vipgoci_options_recognized();
+
+		$this->assertNotCount(
+			0,
+			$options_recognized_arr,
+			'No options returned by vipgoci_options_recognized()'
+		);
+
+		$this->assertTrue(
+			in_array(
+				'help',
+				$options_recognized_arr,
+				true
+			),
+			'"help" not in array returned by vipgoci_options_recognized()'
+		);
+	}
+}
