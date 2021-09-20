6740043e95a83ddd7ca298300f81b9894ad25b49
Ariana Kataoka <aris.kataoka@gmail.com>
2021-09-20 11:54:13 +1000

    Add PHPUnit configuration file

---
+++ tests/unit/DummyTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace Vipgoci\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @todo remove me
+ */
+final class DummyTest extends TestCase {
+	/**
+	 * @covers :: nothing
+	 * This is a dummy test
+	 */
+	public function testMyDummyClass(): void {
+		$this->assertTrue(true);
+	}
+}
