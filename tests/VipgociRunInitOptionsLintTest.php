cc5bf033cf7bd64ce1334ab1ba1e9c19b25a8671
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2021-09-28 12:58:09 +0000

    Test for vipgoci_run_init_options_lint()

---
+++ tests/VipgociRunInitOptionsLintTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Vipgoci\tests;
+
+require_once( __DIR__ . '/IncludesForTests.php' );
+
+use PHPUnit\Framework\TestCase;
+
+// phpcs:disable PSR1.Files.SideEffects
+
+final class VipgociRunInitOptionsLintTest extends TestCase {
+	protected function setUp() :void {
+		$this->options = array(
+			'lint'                                   => null,
+			'lint-skip-folders-in-repo-options-file' => null,
+			'lint-skip-folders'                      => '/folder1/folder2/,folder3/folder4',
+		);
+	}
+
+	protected function tearDown() :void {
+		unset( $this->options );
+	}
+
+	/**
+	 * @covers ::vipgoci_run_init_options_lint
+	 */
+	public function testRunInitOptionsLintDefault() {
+		vipgoci_run_init_options_lint(
+			$this->options
+		);
+
+		$this->assertSame(
+			array(
+				'lint'                                   => true,
+				'lint-skip-folders-in-repo-options-file' => false,
+				'lint-skip-folders'                      => array( 'folder1/folder2', 'folder3/folder4' ),
+			),
+			$this->options
+		);
+	}
+
+	/**
+	 * @covers ::vipgoci_run_init_options_lint
+	 */
+	public function testRunInitOptionsLintCustom() {
+		$this->options['lint'] = 'false';
+		$this->options['lint-skip-folders-in-repo-options-file'] = 'true';
+
+		vipgoci_run_init_options_lint(
+			$this->options
+		);
+
+		$this->assertSame(
+			array(
+				'lint'                                   => false,
+				'lint-skip-folders-in-repo-options-file' => true,
+				'lint-skip-folders'                      => array( 'folder1/folder2', 'folder3/folder4' ),
+			),
+			$this->options
+		);
+	}
+}
