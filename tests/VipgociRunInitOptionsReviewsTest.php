d2675c5970fe5bf21c5ad6a4273025f48b2bc556
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2021-09-28 15:09:09 +0000

    Tests for vipgoci_run_init_options_reviews()

---
+++ tests/VipgociRunInitOptionsReviewsTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Vipgoci\tests;
+
+require_once( __DIR__ . '/IncludesForTests.php' );
+
+use PHPUnit\Framework\TestCase;
+
+// phpcs:disable PSR1.Files.SideEffects
+
+final class VipgociRunInitOptionsReviewsTest extends TestCase {
+	protected function setUp() :void {
+		$this->options = array();
+	}
+
+	protected function tearDown() :void {
+		unset( $this->options );
+	}
+
+	/**
+	 * @covers ::vipgoci_run_init_options_reviews
+	 */
+	public function testRunInitOptionsReviewsDefault() {
+		vipgoci_run_init_options_reviews(
+			$this->options
+		);
+
+		$this->assertSame(
+			array(
+				'review-comments-sort'                        => false,
+				'review-comments-include-severity'            => false,
+				'review-comments-max'                         => 10,
+				'review-comments-total-max'                   => 200,
+				'review-comments-ignore'                      => array(),
+				'dismiss-stale-reviews'                       => false,
+				'dismissed-reviews-repost-comments'           => true,
+				'dismissed-reviews-exclude-reviews-from-team' => array(),
+			),
+			$this->options
+		);
+	}
+
+	/**
+	 * @covers ::vipgoci_run_init_options_reviews
+	 */
+	public function testRunInitOptionsReviewsCustom() {
+		$this->options = array(
+			'review-comments-sort'              => 'true',
+			'review-comments-include-severity'  => 'true',
+			'review-comments-max'               => '50',
+			'review-comments-total-max'         => '100',
+			'review-comments-ignore'            => 'comment1|||comment2',
+			'dismiss-stale-reviews'             => 'true',
+			'dismissed-reviews-repost-comments' => 'false',
+		);
+
+		vipgoci_run_init_options_reviews(
+			$this->options
+		);
+
+		$this->assertSame(
+			array(
+				'review-comments-sort'                        => true,
+				'review-comments-include-severity'            => true,
+				'review-comments-max'                         => 50,
+				'review-comments-total-max'                   => 100,
+				'review-comments-ignore'                      => array( 'comment1', 'comment2' ),
+				'dismiss-stale-reviews'                       => true,
+				'dismissed-reviews-repost-comments'           => false,
+				'dismissed-reviews-exclude-reviews-from-team' => array(),
+			),
+			$this->options
+		);
+	}
+}
