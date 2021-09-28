485af53e73e24fd2c35ee8ed7fae1f1a57df5ee2
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2021-09-28 14:16:25 +0000

    Adding test for vipgoci_run_init_options_hashes_options()

---
+++ tests/VipgociRunInitOptionsHashesOptionsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Vipgoci\tests;
+
+require_once( __DIR__ . '/IncludesForTests.php' );
+
+use PHPUnit\Framework\TestCase;
+
+// phpcs:disable PSR1.Files.SideEffects
+
+final class VipgociRunInitOptionsHashesOptionsTest extends TestCase {
+	protected function setUp() :void {
+		$this->options = array();
+	}
+
+	protected function tearDown() :void {
+		unset( $this->options );
+	}
+
+	/**
+	 * @covers ::vipgoci_run_init_options_hashes_options
+	 */
+	public function testRunInitOptionsHashesOptionsDefault() {
+		$this->options = array(
+			'hashes-api'          => null,
+			'hashes-api-url'      => 'https://api.test.local/v1/api',
+			'hashes-oauth-param1' => ' value1  ',
+			'hashes-oauth-param2' => '  value2 ',
+		);
+
+		$this->hashes_oauth_arguments = array(
+			'hashes-oauth-param1',
+			'hashes-oauth-param2',
+		);
+
+		vipgoci_run_init_options_hashes_options(
+			$this->options,
+			$this->hashes_oauth_arguments
+		);
+
+		$this->assertSame(
+			array(
+				'hashes-api'          => false,
+				'hashes-api-url'      => 'https://api.test.local/v1/api',
+				'hashes-oauth-param1' => 'value1',
+				'hashes-oauth-param2' => 'value2',
+			),
+			$this->options
+		);
+	}
+
+	/**
+	 * @covers ::vipgoci_run_init_options_hashes_options
+	 */
+	public function testRunInitOptionsHashesOptionsCustom() {
+		$this->options = array(
+			'hashes-api'          => 'true',
+			'hashes-api-url'      => 'https://api.test.local/v1/api',
+			'hashes-oauth-param1' => ' value1  ',
+			'hashes-oauth-param2' => '  value2 ',
+		);
+
+		$this->hashes_oauth_arguments = array(
+			'hashes-oauth-param1',
+			'hashes-oauth-param2',
+		);
+
+		vipgoci_run_init_options_hashes_options(
+			$this->options,
+			$this->hashes_oauth_arguments
+		);
+
+		$this->assertSame(
+			array(
+				'hashes-api'          => true,
+				'hashes-api-url'      => 'https://api.test.local/v1/api',
+				'hashes-oauth-param1' => 'value1',
+				'hashes-oauth-param2' => 'value2',
+			),
+			$this->options
+		);
+	}
+}
