ec16a001a71de475d87f742a4e986ceeb655d8bb
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-02 18:14:03 +0000

    Removing VIPGOCI_UNIT_TESTS_TEST_ID_KEY, lives in unit-test helper now

--- tests/integration/IncludesForTests.php
+++ tests/integration/IncludesForTests.php
@@ -10,8 +10,6 @@ if ( ! defined( 'VIPGOCI_UNIT_TESTS_INI_DIR_PATH' ) ) {
 	define( 'VIPGOCI_UNIT_TESTS_INI_DIR_PATH', dirname( __DIR__ ) );
 }
 
-define( 'VIPGOCI_UNIT_TESTS_TEST_ID_KEY', 'vipgoci_unittests_test_ids' );
-
 require_once( __DIR__ . '/IncludesForTestsOutputControl.php' );
 require_once( __DIR__ . '/IncludesForTestsConfig.php' );
 require_once( __DIR__ . '/IncludesForTestsRepo.php' );
