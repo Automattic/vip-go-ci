03a85e8278602b06eaedf3d31d0d16ea9a7f8405
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 00:38:24 +0000

    Coding style fixes

--- tests/unit/A00IrcApiAlertQueueTest.php
+++ tests/unit/A00IrcApiAlertQueueTest.php
@@ -4,28 +4,32 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
 /**
+ * Test IRC queue function.
+ *
  * @runTestsInSeparateProcesses
  * @preserveGlobalState disabled
  */
 final class A00IrcApiAlertQueueTest extends TestCase {
+	/**
+	 * Require correct file.
+	 */
 	public function setUp() :void {
 		/*
 		 * Ensure this file is required on execution
 		 * of the test itself. This test is run in separate
-		 * process so other tests are unaffected 
+		 * process so other tests are unaffected
 		 * by this require. This is needed to ensure function
 		 * declarations are not attempted multiple times.
 		 */
-		require_once( __DIR__ . '/../../other-web-services.php' );
+		require_once __DIR__ . '/../../other-web-services.php';
 	}
 
 	/**
+	 * Test if IRC queue functions correctly.
+	 *
 	 * @covers ::vipgoci_irc_api_alert_queue
 	 */
 	public function testIrcQueue1() {
