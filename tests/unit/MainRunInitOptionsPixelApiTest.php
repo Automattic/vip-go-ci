335545fda45af3b3f93669d35120e16a1726b7f4
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 13:05:54 +0000

    Coding style fixes

--- tests/unit/MainRunInitOptionsPixelApiTest.php
+++ tests/unit/MainRunInitOptionsPixelApiTest.php
@@ -4,25 +4,36 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . '/../../main.php' );
-require_once( __DIR__ . '/../../options.php' );
-require_once( __DIR__ . '/../../misc.php' );
+require_once __DIR__ . '/../../main.php';
+require_once __DIR__ . '/../../options.php';
+require_once __DIR__ . '/../../misc.php';
 
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
+/**
+ * Check if Pixel API default options are correctly provided.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MainRunInitOptionsPixelApiTest extends TestCase {
+	/**
+	 * Set up variable.
+	 */
 	protected function setUp() :void {
-		$this->options = array(
-		);
+		$this->options = array();
 	}
 
+	/**
+	 * Clean up variable.
+	 */
 	protected function tearDown() :void {
 		unset( $this->options );
 	}
 
 	/**
+	 * Check if Pixel API default options are correctly provided.
+	 *
 	 * @covers ::vipgoci_run_init_options_pixel_api
 	 */
 	public function testRunInitOptionsPixelApiDefault() :void {
@@ -39,12 +50,14 @@ final class MainRunInitOptionsPixelApiTest extends TestCase {
 	}
 
 	/**
+	 * Check if Pixel API options are correctly parsed.
+	 *
 	 * @covers ::vipgoci_run_init_options_pixel_api
 	 */
 	public function testRunInitOptionsPixelApiCustom() :void {
 		$this->options = array(
 			'pixel-api-url'         => 'https://api.test.local/v1/api  ',
-			'pixel-api-groupprefix' => '  _prefix1  '
+			'pixel-api-groupprefix' => '  _prefix1  ',
 		);
 
 		vipgoci_run_init_options_pixel_api(
@@ -54,7 +67,7 @@ final class MainRunInitOptionsPixelApiTest extends TestCase {
 		$this->assertSame(
 			array(
 				'pixel-api-url'         => 'https://api.test.local/v1/api',
-				'pixel-api-groupprefix' => '_prefix1'
+				'pixel-api-groupprefix' => '_prefix1',
 			),
 			$this->options
 		);
