1392d90e463962ad37673d5fa1d65f903fd508a5
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 00:24:11 +0000

    Coding style fixes

--- tests/integration/Skeleton.php
+++ tests/integration/Skeleton.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Integration;
 
-require_once( __DIR__ . '/IncludesForTests.php' );
+require_once __DIR__ . '/IncludesForTests.php';
 
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
+/**
+ * Comment to describe the test.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class Skeleton extends TestCase {
 	/**
+	 * Short description of the function.
+	 *
 	 * @covers ::
 	 */
-	public function test (): void {
+	public function testName(): void {
 	}
 }
