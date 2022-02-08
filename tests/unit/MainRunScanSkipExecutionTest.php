4e9cc82188c1d56c63909cb7367d5429b2bcceda
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 13:16:22 +0000

    Coding style fixes

--- tests/unit/MainRunScanSkipExecutionTest.php
+++ tests/unit/MainRunScanSkipExecutionTest.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . '/helper/IndicateTestId.php' );
+require_once __DIR__ . '/helper/IndicateTestId.php';
 
-require_once( __DIR__ . './../../defines.php' );
-require_once( __DIR__ . './../../main.php' );
-require_once( __DIR__ . './../../misc.php' );
+require_once __DIR__ . './../../defines.php';
+require_once __DIR__ . './../../main.php';
+require_once __DIR__ . './../../misc.php';
 
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
+/**
+ * Check if skip execution functionality works.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MainRunScanSkipExecutionTest extends TestCase {
+	/**
+	 * Set up variable, indicate what test is running.
+	 */
 	protected function setUp(): void {
 		/*
 		 * Indicate that this particular test is running,
@@ -26,11 +33,10 @@ final class MainRunScanSkipExecutionTest extends TestCase {
 		$this->options = array();
 	}
 
+	/**
+	 * Remove the indication and clear variable.
+	 */
 	protected function tearDown(): void {
-		/*
-	 	 * We are no longer running the test,
-		 * remove the indication.
-		 */
 		vipgoci_unittests_remove_indication_for_test_id( 'MainRunScanSkipExecutionTest' );
 
 		unset( $this->options );
