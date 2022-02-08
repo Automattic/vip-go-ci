f3a0a70dd5578dd66253472c0add379d8af093da
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 13:12:09 +0000

    Coding style fixes

--- tests/unit/MainRunInitOptionsRepoMetaApiTest.php
+++ tests/unit/MainRunInitOptionsRepoMetaApiTest.php
@@ -4,25 +4,37 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . '/../../main.php' );
-require_once( __DIR__ . '/../../options.php' );
-require_once( __DIR__ . '/../../misc.php' );
+require_once __DIR__ . '/../../main.php';
+require_once __DIR__ . '/../../options.php';
+require_once __DIR__ . '/../../misc.php';
 
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
+/**
+ * Check if repo-meta API options are handled
+ * correctly.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MainRunInitOptionsRepoMetaApiTest extends TestCase {
+	/**
+	 * Set up variable.
+	 */
 	protected function setUp() :void {
-		$this->options = array(
-		);
+		$this->options = array();
 	}
 
+	/**
+	 * Clear variable.
+	 */
 	protected function tearDown() :void {
 		unset( $this->options );
 	}
 
 	/**
+	 * Check if repo-meta API default options are correctly provided.
+	 *
 	 * @covers ::vipgoci_run_init_options_repo_meta_api
 	 */
 	public function testRunInitOptionsRepoMetaApiDefault() :void {
@@ -41,13 +53,15 @@ final class MainRunInitOptionsRepoMetaApiTest extends TestCase {
 	}
 
 	/**
+	 * Check if repo-meta API custom options are correctly parsed.
+	 *
 	 * @covers ::vipgoci_run_init_options_repo_meta_api
 	 */
 	public function testRunInitOptionsRepoMetaApiCustom() :void {
 		$this->options = array(
 			'repo-meta-api-base-url'     => 'https://api.test.local/v1/api  ',
 			'repo-meta-api-user-id'      => '3500',
-			'repo-meta-api-access-token' => '   test  ',	
+			'repo-meta-api-access-token' => '   test  ',
 		);
 
 		vipgoci_run_init_options_repo_meta_api(
