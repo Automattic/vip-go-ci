c3d00780ed6d36f9e49512bb41b272508d084323
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 13:02:56 +0000

    Coding style fixes

--- tests/unit/MainRunInitOptionsLintTest.php
+++ tests/unit/MainRunInitOptionsLintTest.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . '/../../main.php' );
-require_once( __DIR__ . '/../../options.php' );
-require_once( __DIR__ . '/../../misc.php' );
+require_once __DIR__ . '/../../main.php';
+require_once __DIR__ . '/../../options.php';
+require_once __DIR__ . '/../../misc.php';
 
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
+/**
+ * Check if options relating to linting
+ * are correctly parsed and set.
+*
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MainRunInitOptionsLintTest extends TestCase {
+	/**
+	 * Set up variable.
+	 */
 	protected function setUp() :void {
 		$this->options = array(
 			'lint'                                   => null,
@@ -22,11 +30,17 @@ final class MainRunInitOptionsLintTest extends TestCase {
 		);
 	}
 
+	/**
+	 * Clear variable.
+	 */
 	protected function tearDown() :void {
 		unset( $this->options );
 	}
 
 	/**
+	 * Check if options relating to linting
+	 * are correctly parsed and set.
+	 *
 	 * @covers ::vipgoci_run_init_options_lint
 	 */
 	public function testRunInitOptionsLintDefault() :void {
@@ -37,7 +51,7 @@ final class MainRunInitOptionsLintTest extends TestCase {
 		$this->assertSame(
 			array(
 				'lint'                                   => true,
-                                'lint-modified-files-only'               => true,
+				'lint-modified-files-only'               => true,
 				'lint-skip-folders-in-repo-options-file' => false,
 				'lint-skip-folders'                      => array( 'folder1/folder2', 'folder3/folder4' ),
 			),
@@ -46,11 +60,14 @@ final class MainRunInitOptionsLintTest extends TestCase {
 	}
 
 	/**
+	 * Check if options relating to linting
+	 * are correctly parsed and set.
+	 *
 	 * @covers ::vipgoci_run_init_options_lint
 	 */
 	public function testRunInitOptionsLintCustom() :void {
-		$this->options['lint'] = 'false';
-		$this->options['lint-modified-files-only'] = 'false';
+		$this->options['lint']                                   = 'false';
+		$this->options['lint-modified-files-only']               = 'false';
 		$this->options['lint-skip-folders-in-repo-options-file'] = 'true';
 
 		vipgoci_run_init_options_lint(
