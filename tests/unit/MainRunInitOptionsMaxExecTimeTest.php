3efbacb89d6d68a63a91e0f1977a13401b103459
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 13:04:57 +0000

    Coding style fixes

--- tests/unit/MainRunInitOptionsMaxExecTimeTest.php
+++ tests/unit/MainRunInitOptionsMaxExecTimeTest.php
@@ -4,26 +4,40 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . '/../../main.php' );
-require_once( __DIR__ . '/../../options.php' );
-require_once( __DIR__ . '/../../misc.php' );
+require_once __DIR__ . '/../../main.php';
+require_once __DIR__ . '/../../options.php';
+require_once __DIR__ . '/../../misc.php';
 
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
+/**
+ * Check if maximum execution time option is
+ * parsed correctly.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MainRunInitOptionsMaxExecTimeTest extends TestCase {
+	/**
+	 * Set up variable.
+	 */
 	protected function setUp() :void {
 		$this->options = array(
 			'max-exec-time' => 100,
 		);
 	}
 
+	/**
+	 * Clear variable.
+	 */
 	protected function tearDown() :void {
 		unset( $this->options );
 	}
 
 	/**
+	 * Check if maximum execution time option is
+	 * parsed correctly.
+	 *
 	 * @covers ::vipgoci_run_init_options_max_exec_time
 	 */
 	public function testRunInitOptionsMaxExecTime() :void {
