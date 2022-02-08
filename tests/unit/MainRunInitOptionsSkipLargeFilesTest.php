8e201fb1d847c980ca4cc03ff039e8e6f0bb0577
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 13:15:04 +0000

    Coding style fixes

--- tests/unit/MainRunInitOptionsSkipLargeFilesTest.php
+++ tests/unit/MainRunInitOptionsSkipLargeFilesTest.php
@@ -4,26 +4,39 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . '/../../main.php' );
-require_once( __DIR__ . '/../../options.php' );
-require_once( __DIR__ . '/../../misc.php' );
-require_once( __DIR__ . '/../../defines.php' );
+require_once __DIR__ . '/../../main.php';
+require_once __DIR__ . '/../../options.php';
+require_once __DIR__ . '/../../misc.php';
+require_once __DIR__ . '/../../defines.php';
 
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
+/**
+ * Check if skip-large files options
+ * are correctly handled.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MainRunInitOptionsSkipLargeFilesTest extends TestCase {
+	/**
+	 * Set up variable.
+	 */
 	protected function setUp() :void {
-		$this->options = array(
-		);
+		$this->options = array();
 	}
 
+	/**
+	 * Clear variable.
+	 */
 	protected function tearDown() :void {
 		unset( $this->options );
 	}
 
 	/**
+	 * Check if default --skip-large-files* options are
+	 * correctly provided.
+	 *
 	 * @covers ::vipgoci_run_init_options_skip_large_files
 	 */
 	public function testRunInitOptionsSkipLargeFilesDefault() :void {
@@ -41,10 +54,13 @@ final class MainRunInitOptionsSkipLargeFilesTest extends TestCase {
 	}
 
 	/**
+	 * Check if custom --skip-large-files* options are
+	 * correctly parsed.
+	 *
 	 * @covers ::vipgoci_run_init_options_skip_large_files
 	 */
 	public function testRunInitOptionsSkipLargeFilesCustom() :void {
-		$this->options['skip-large-files'] = 'false';
+		$this->options['skip-large-files']       = 'false';
 		$this->options['skip-large-files-limit'] = '30000';
 
 		vipgoci_run_init_options_skip_large_files(
