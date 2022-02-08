8d5730c19aadba05dad5fd3712f9b4637a3620bb
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-08 00:49:56 +0000

    Coding style fixes

--- tests/unit/Skeleton.php
+++ tests/unit/Skeleton.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-// Note: require_once() calls should be in setUp()
+// Note: require_once() calls should be in setUp().
 
 use PHPUnit\Framework\TestCase;
 
 // phpcs:disable PSR1.Files.SideEffects
 
 /**
+ * Comment to describe the test.
+ *
  * See README.md for details.
  *
  * @runTestsInSeparateProcesses
@@ -23,12 +25,14 @@ final class Skeleton extends TestCase {
 	 * All files should be required here. See README.md.
 	 */
 	protected function setUp() :void {
-		require_once( __DIR__ . './../../FILE-TO-BE-TESTED' );
+		require_once __DIR__ . './../../FILE-TO-BE-TESTED';
 	}
 
 	/**
+	 * Short description of the function.
+	 *
 	 * @covers ::
 	 */
-	public function test (): void {
+	public function testName(): void {
 	}
 }
