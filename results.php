2cef50891b144d206c1229c244802221c402b7f7
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-17 19:56:00 +0000

    Moving functions between files

--- results.php
+++ results.php
@@ -788,3 +788,264 @@ function vipgoci_results_sort_by_severity(
 	}
 }
 
+/**
+ * Check if the specified comment exists
+ * within an array of other comments --
+ * this is used to understand if the specific
+ * comment has already been submitted earlier.
+ *
+ * @param string $file_issue_path    Path to file.
+ * @param int    $file_issue_line    Line number in file.
+ * @param string $file_issue_comment Comment to look for.
+ * @param array  $comments_made      Array of comments to search in.
+ *
+ * @return bool True if comment exists, false if not.
+ */
+function vipgoci_github_comment_match(
+	string $file_issue_path,
+	int $file_issue_line,
+	string $file_issue_comment,
+	array $comments_made
+) {
+	/*
+	 * Construct an index-key made of file:line.
+	 */
+	$comment_index_key =
+		$file_issue_path .
+		':' .
+		$file_issue_line;
+
+	if ( ! isset(
+		$comments_made[ $comment_index_key ]
+	) ) {
+		/*
+		 * No match on index-key within the
+		 * associative array -- the comment has
+		 * not been made, so return false.
+		 */
+		return false;
+	}
+
+	/*
+	 * Some comment matching the file and line-number
+	 * was found -- figure out if it is definately the
+	 * same comment.
+	 */
+	foreach (
+		$comments_made[ $comment_index_key ] as
+		$comment_made
+	) {
+		/*
+		 * The comment might contain formatting, such
+		 * as "Warning: ..." -- remove all of that.
+		 */
+		$comment_made_body = str_replace(
+			array( '**', 'Warning', 'Error', 'Info', ':no_entry_sign:', ':warning:', ':information_source:' ),
+			array( '', '', '', '', '' ),
+			$comment_made->body
+		);
+
+		/*
+		 * The comment might include severity level
+		 * -- remove that.
+		 */
+		$comment_made_body = preg_replace(
+			'/\( severity \d{1,2} \)/',
+			'',
+			$comment_made_body
+		);
+
+		/*
+		 * The comment might be prefixed with ': ',
+		 * remove that as well.
+		 */
+		$comment_made_body = ltrim(
+			$comment_made_body,
+			': '
+		);
+
+		/*
+		 * The comment might include PHPCS source
+		 * of the error at the end (e.g.
+		 * "... (*WordPress.WP.AlternativeFunctions.json_encode_json_encode*)."
+		 * -- remove the source, the brackets and the ending dot.
+		 */
+		$comment_made_body = preg_replace(
+			'/ \([\*_\.a-zA-Z0-9]+\)\.$/',
+			'',
+			$comment_made_body
+		);
+
+		/*
+		 * Transform string to lowercase,
+		 * remove ending '.' just in case if
+		 * not removed earlier.
+		 */
+		$comment_made_body = strtolower(
+			$comment_made_body
+		);
+
+		$comment_made_body = rtrim(
+			$comment_made_body,
+			'.'
+		);
+
+		/*
+		 * Transform the string to lowercase,
+		 * and remove potential '.' at the end
+		 * of it.
+		 */
+		$file_issue_comment = strtolower(
+			$file_issue_comment
+		);
+
+		$file_issue_comment = rtrim(
+			$file_issue_comment,
+			'.'
+		);
+
+		/*
+		 * Check if comments match, including
+		 * if we need to HTML-encode our new comment
+		 * (GitHub encodes their comments when
+		 * returning them.
+		 */
+		if (
+			(
+				$comment_made_body ===
+				$file_issue_comment
+			)
+			||
+			(
+				htmlentities( $file_issue_comment ) ===
+				$comment_made_body
+			)
+		) {
+			/* Comment found, return true. */
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Filter out any issues in the code that were not
+ * touched up on by the changed lines -- i.e., any issues
+ * that existed prior to the change.
+ *
+ * @param string $file_name           Name of file being processed.
+ * @param array  $file_issues_arr     List of issues in file.
+ * @param array  $file_blame_log      Git blame log for file.
+ * @param array  $pr_item_commits     List of commits.
+ * @param array  $file_relative_lines Relative line numbers for file and commit.
+ *
+ * @return array Only lines altered in the file.
+ */
+function vipgoci_issues_filter_irrellevant(
+	string $file_name,
+	array $file_issues_arr,
+	array $file_blame_log,
+	array $pr_item_commits,
+	array $file_relative_lines
+) :array {
+	/*
+	 * Filter out any issues
+	 * that are due to commits outside
+	 * of the Pull-Request
+	 */
+
+	$file_blame_log_filtered =
+		vipgoci_blame_filter_commits(
+			$file_blame_log,
+			$pr_item_commits
+		);
+
+	$file_issues_ret = array();
+
+	/*
+	 * Loop through all the issues affecting
+	 * this particular file
+	 */
+	foreach (
+		$file_issues_arr[ $file_name ] as
+			$file_issue_key =>
+			$file_issue_val
+	) {
+		$keep_issue = false;
+
+		/*
+		 * Filter out issues outside of the blame log
+		 */
+
+		foreach ( $file_blame_log_filtered as $blame_log_item ) {
+			if (
+				$blame_log_item['line_no'] ===
+					$file_issue_val['line']
+			) {
+				$keep_issue = true;
+			}
+		}
+
+		if ( false === $keep_issue ) {
+			continue;
+		}
+
+		unset( $keep_issue );
+
+		/*
+		 * Filter out any issues that are outside
+		 * of the current patch
+		 */
+
+		if ( ! isset(
+			$file_relative_lines[ $file_issue_val['line'] ]
+		) ) {
+			continue;
+		}
+
+		// Passed all tests, keep this issue.
+		$file_issues_ret[] = $file_issue_val;
+	}
+
+	return $file_issues_ret;
+}
+
+/**
+ * In case of some issues being reported in duplicate
+ * by PHPCS, remove those. Only issues reported
+ * twice in the same file on the same line are considered
+ * a duplicate.
+ *
+ * @param array $file_issues_arr Issues to process.
+ *
+ * @return array Processed issues, without duplicates.
+ */
+function vipgoci_issues_filter_duplicate(
+	array $file_issues_arr
+) :array {
+	$issues_hashes       = array();
+	$file_issues_arr_new = array();
+
+	foreach (
+		$file_issues_arr as
+			$issue_item_key => $issue_item_value
+	) {
+		$issue_item_hash = md5(
+			$issue_item_value['message']
+		)
+		. ':' .
+		$issue_item_value['line'];
+
+		if ( in_array( $issue_item_hash, $issues_hashes, true ) ) {
+			continue;
+		}
+
+		$issues_hashes[] = $issue_item_hash;
+
+		$file_issues_arr_new[] = $issue_item_value;
+	}
+
+	return $file_issues_arr_new;
+}
+
