b62bd802f98e7e90baa6ac11a1ebcfbc1d8586c3
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-21 14:52:51 +0000

    Rename test, add testing for empty string value, apply WP CS.

--- tests/unit/OptionsArrayHandleTest.php
+++ tests/unit/OptionsArrayHandleTest.php
@@ -1,22 +1,64 @@
 <?php
+/**
+ * Test vipgoci_option_array_handle() function, which
+ * parses options and places into an array.
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../options.php' );
-
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
-final class VipgociOptionsArrayHandleTest extends TestCase {
+/**
+ * Class that implements all the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+final class OptionsArrayHandleTest extends TestCase {
 	/**
+	 * Setup function.
+	 *
+	 * All files should be required here. See README.md.
+	 */
+	protected function setUp() :void {
+		require_once __DIR__ . './../../options.php';
+	}
+
+	/**
+	 * Test when option is an empty string.
+	 *
 	 * @covers ::vipgoci_option_array_handle
 	 */
-	public function testOptionsArrayHandle1() {
+	public function testOptionsArrayHandle1() :void {
 		$options = array(
+			'mytestoption' => '',
 		);
+
+		vipgoci_option_array_handle(
+			$options,
+			'mytestoption',
+			array(),
+			null,
+			','
+		);
+
+		$this->assertSame(
+			array(),
+			$options['mytestoption']
+		);
+	}
+
+	/**
+	 * Test when option is empty.
+	 *
+	 * @covers ::vipgoci_option_array_handle
+	 */
+	public function testOptionsArrayHandle2() :void {
+		$options = array();
 
 		vipgoci_option_array_handle(
 			$options,
@@ -35,35 +77,12 @@ final class VipgociOptionsArrayHandleTest extends TestCase {
 	}
 
 	/**
+	 * Test when there are multiple values, comma separated,
+	 * and comma is the separator.
+	 *
 	 * @covers ::vipgoci_option_array_handle
 	 */
-	public function testOptionsArrayHandle2() {
-		$options = array(
-			'mytestoption' => 'myvalue1,myvalue2,myvalue3',
-		);
-
-		vipgoci_option_array_handle(
-			$options,
-			'mytestoption',
-			'myvalue',
-			null,
-			','
-		);
-
-		$this->assertSame(
-			array(
-				'myvalue1',
-				'myvalue2',
-				'myvalue3',
-			),
-			$options['mytestoption']
-		);
-	}
-
-	/**
-	 * @covers ::vipgoci_option_array_handle
-	 */
-	public function testOptionsArrayHandle3() {
+	public function testOptionsArrayHandle3() :void {
 		$options = array(
 			'mytestoption' => 'myvalue1,myvalue2,MYVALUE3',
 		);
@@ -80,16 +99,20 @@ final class VipgociOptionsArrayHandleTest extends TestCase {
 			array(
 				'myvalue1',
 				'myvalue2',
-				'myvalue3', // should be transformed to lower-case by default
+				'myvalue3', // Should be transformed to lower-case by default.
 			),
 			$options['mytestoption']
 		);
 	}
 
 	/**
+	 * Test if array is handled just like
+	 * in the test above, but values should
+	 * not be transformed to lower case.
+	 *
 	 * @covers ::vipgoci_option_array_handle
 	 */
-	public function testOptionsArrayHandle4() {
+	public function testOptionsArrayHandle4() :void {
 		$options = array(
 			'mytestoption' => 'myvalue1,myvalue2,MYVALUE3',
 		);
@@ -100,7 +123,7 @@ final class VipgociOptionsArrayHandleTest extends TestCase {
 			'myvalue',
 			null,
 			',',
-			false // do not strtolower()
+			false // Do not strtolower() values.
 		);
 
 		$this->assertSame(
