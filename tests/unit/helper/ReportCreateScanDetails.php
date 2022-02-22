da40054c0056a36ed154c2dcba5ff1f0a3d4afae
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-02-22 19:02:53 +0000

    Adding helper functions for ReportCreateScanDetails* tests.

---
+++ tests/unit/helper/ReportCreateScanDetails.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Helper function implementation for
+ * ReportCreateScanDetails* tests.
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+/**
+ * Helper function that does nothing.
+ *
+ * @param string $php_path Path to PHP.
+ *
+ * @return string Version number (fixed).
+ */
+function vipgoci_util_php_interpreter_get_version(
+	string $php_path
+) :string {
+	return '7.4.3';
+}
+
+/**
+ * Helper function that does nothing.
+ *
+ * @param string $phpcs_path Path to PHPCS.
+ * @param string $php_path   Path to PHP.
+ *
+ * @return string Version number (fixed).
+ */
+function vipgoci_phpcs_get_version(
+	string $phpcs_path,
+	string $php_path
+) :string {
+	return '3.5.5';
+}
+
+/**
+ * Helper function that does nothing.
+ *
+ * @param array $options Options needed.
+ *
+ * @return void
+ */
+function vipgoci_options_sensitive_clean(
+	array $options
+) :void {
+	return $options;
+}
+
