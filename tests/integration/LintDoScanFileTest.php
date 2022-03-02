47a01bc103a2717c46346ff45302f682d17ec6b2
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-03-02 14:41:55 +0000

    Update test, apply WP CS.

---
+++ tests/integration/LintDoScanFileTest.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Test vipgoci_lint_do_scan_file() function.
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+final class LintDoScanFileTest extends TestCase {
+	/**
+	 * Options variable for the tests.
+	 *
+	 * @var $options_php
+	 */
+	private array $options_php = array(
+		'lint-php-path' => null,
+	);
+
+	/**
+	 * Setup function. Require files, set options variable.
+	 *
+	 * @return void
+	 */
+	protected function setUp(): void {
+		require_once __DIR__ . '/IncludesForTests.php';
+
+		vipgoci_unittests_get_config_values(
+			'lint-scan',
+			$this->options_php
+		);
+	}
+
+	/**
+	 * Teardown function. Unset variables.
+	 *
+	 * @return void
+	 */
+	protected function tearDown(): void {
+		unset( $this->options_php );
+	}
+
+	/**
+	 * Test PHP linting when there are no issues.
+	 *
+	 * @covers ::vipgoci_lint_do_scan_file
+	 */
+	public function testLintDoScan1() {
+		$options_test = vipgoci_unittests_options_test(
+			$this->options_php,
+			array(),
+			$this
+		);
+
+		if ( -1 === $options_test ) {
+			return;
+		}
+
+		$php_file_path = vipgoci_save_temp_file(
+			'test-lint-do-scan-1',
+			'php',
+			'<?php ' . PHP_EOL . 'echo "foo";' . PHP_EOL
+		);
+
+		$php_file_name = pathinfo(
+			$php_file_path,
+			PATHINFO_FILENAME
+		);
+
+		vipgoci_unittests_output_suppress();
+
+		$ret = vipgoci_lint_do_scan_file(
+			$this->options_php['lint-php-path'],
+			$php_file_path
+		);
+
+		vipgoci_unittests_output_unsuppress();
+
+		$this->assertSame(
+			array(
+				'No syntax errors detected in ' . $php_file_path,
+			),
+			$ret
+		);
+	}
+
+	/**
+	 * Test PHP linting when there are issues.
+	 *
+	 * @covers ::vipgoci_lint_do_scan_file
+	 */
+	public function testLintDoScan2() {
+		$options_test = vipgoci_unittests_options_test(
+			$this->options_php,
+			array(),
+			$this
+		);
+
+		if ( -1 === $options_test ) {
+			return;
+		}
+
+		$php_file_path = vipgoci_save_temp_file(
+			'test-lint-do-scan-2',
+			'php',
+			'<?php ' . PHP_EOL . 'echo "foo"' . PHP_EOL
+		);
+
+		$php_file_name = pathinfo(
+			$php_file_path,
+			PATHINFO_FILENAME
+		);
+
+		vipgoci_unittests_output_suppress();
+
+		$ret = vipgoci_lint_do_scan_file(
+			$this->options_php['lint-php-path'],
+			$php_file_path
+		);
+
+		vipgoci_unittests_output_unsuppress();
+
+		$ret[0] = vipgoci_unittests_php_syntax_error_compat(
+			$ret[0]
+		);
+
+		$this->assertSame(
+			array(
+				"PHP Parse error:  syntax error, unexpected end of file, expecting ',' or ';' in " . $php_file_path . ' on line 3',
+				'Errors parsing ' . $php_file_path,
+			),
+			$ret
+		);
+	}
+}
