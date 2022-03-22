f2d228add7eb8ed241f8f4b590ec029fea4508b3
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-03-22 16:56:20 +0000

    Test using two headers with same name.

--- tests/unit/GitHubApiCurlHeadersTest.php
+++ tests/unit/GitHubApiCurlHeadersTest.php
@@ -78,6 +78,7 @@ final class GitHubApiCurlHeadersTest extends TestCase {
 
 	/**
 	 * Test using HTTP/2 Status compatibility header.
+	 * Also test using two headers with same name.
 	 *
 	 * @return void
 	 *
@@ -104,6 +105,16 @@ final class GitHubApiCurlHeadersTest extends TestCase {
 			'Location: https://www.mytestdomain2.is/'
 		);
 
+		vipgoci_curl_headers(
+			$ch,
+			'X-Test-Header: value1',
+		);
+
+		vipgoci_curl_headers(
+			$ch,
+			'X-Test-Header: value2',
+		);
+
 		$actual_results = vipgoci_curl_headers(
 			null,
 			null
@@ -111,9 +122,10 @@ final class GitHubApiCurlHeadersTest extends TestCase {
 
 		$this->assertSame(
 			array(
-				'status'   => array( '205' ),
-				'date'     => array( 'Mon, 04 Mar 2020 16:43:35 GMT' ),
-				'location' => array( 'https://www.mytestdomain2.is/' ),
+				'status'        => array( '205' ),
+				'date'          => array( 'Mon, 04 Mar 2020 16:43:35 GMT' ),
+				'location'      => array( 'https://www.mytestdomain2.is/' ),
+				'x-test-header' => array( 'value1', 'value2' ),
 			),
 			$actual_results
 		);
