54cd9a307fb9083d81c7191079922497b92138c2
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-03-23 14:21:34 +0000

    vipgoci_github_wait() renamed to vipgoci_http_api_wait(), use constant.

--- tests/unit/GitHubMiscGitHubWaitTest.php
+++ tests/unit/GitHubMiscGitHubWaitTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Tests the vipgoci_github_wait() function.
+ * Tests the vipgoci_http_api_wait() function.
  *
  * @package Automattic/vip-go-ci
  */
@@ -22,24 +22,24 @@ final class GitHubMiscGitHubWaitTest extends TestCase {
 	 * Setup function. Require files.
 	 */
 	protected function setUp() :void {
-		require_once __DIR__ . '/../../github-misc.php';
+		require_once __DIR__ . '/../../http-functions.php';
 		require_once __DIR__ . '/../../defines.php';
 
 		require_once __DIR__ . '/helper/RuntimeMeasure.php';
 	}
 
 	/**
-	 * Test the vipgoci_github_wait() function by
+	 * Test the vipgoci_http_api_wait() function by
 	 * calling it repeatedly and check if it successfully
 	 * returns after a defined time when it should do so.
 	 *
-	 * @covers ::vipgoci_github_wait
+	 * @covers ::vipgoci_http_api_wait
 	 */
 	public function testGitHubWait(): void {
 		for ( $i = 0; $i < 4; $i++ ) {
 			$time_start = time();
 
-			vipgoci_github_wait();
+			vipgoci_http_api_wait();
 
 			$time_end = time();
 
@@ -49,13 +49,13 @@ final class GitHubMiscGitHubWaitTest extends TestCase {
 				// If first run, should return instantly. Allow for some variation.
 				$this->assertTrue(
 					( ( $time_spent >= 0 ) && ( $time_spent <= 2 ) ),
-					'vipgoci_github_wait() returned earlier or later than expected'
+					'vipgoci_http_api_wait() returned earlier or later than expected'
 				);
 			} else {
 				// On later runs, should wait for a bit. Allow for some variation.
 				$this->assertTrue(
-					( ( $time_spent >= 1 ) && ( $time_spent <= ( VIPGOCI_GITHUB_WAIT_TIME_SECONDS + 1 ) ) ),
-					'vipgoci_github_wait() returned earlier or later than expected'
+					( ( $time_spent >= 1 ) && ( $time_spent <= ( VIPGOCI_HTTP_API_WAIT_TIME_SECONDS + 1 ) ) ),
+					'vipgoci_http_api_wait() returned earlier or later than expected'
 				);
 			}
 		}
