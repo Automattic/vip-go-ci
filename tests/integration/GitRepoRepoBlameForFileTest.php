bc4f0e30f1b95833803277491350477f40c60af3
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-03-30 18:18:27 +0000

    Add and update tests for vipgoci_gitrepo_blame_for_file()

--- tests/integration/GitRepoRepoBlameForFileTest.php
+++ tests/integration/GitRepoRepoBlameForFileTest.php
@@ -1,22 +1,52 @@
 <?php
+/**
+ * Test for vipgoci_gitrepo_blame_for_file() function.
+ *
+ * @package Automattic/vip-go-ci
+ */
 
-require_once( __DIR__ . '/IncludesForTests.php' );
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class GitRepoRepoBlameForFileTest extends TestCase {
-	var $options_git = array(
+	/**
+	 * Git options.
+	 *
+	 * @var $options_git
+	 */
+	private array $options_git = array(
 		'git-path'			=> null,
 		'github-repo-url'		=> null,
 		'repo-owner'			=> null,
 		'repo-name'			=> null,
 	);
 
-	var $options_git_repo_tests = array(
+	/**
+	 * Git repo tests options.
+	 *
+ 	 * @var $options_git_repo_tests
+	 */
+	private array $options_git_repo_tests = array(
 		'commit-test-repo-blame-for-file-2'	=> null,
 	);
 
+	/**
+	 * Setup function. Require files, check out git repository, etc.
+	 *
+	 * @return void
+	 */
 	protected function setUp(): void {
+		require_once __DIR__ . '/IncludesForTests.php';
+
 		vipgoci_unittests_get_config_values(
 			'git',
 			$this->options_git
@@ -31,15 +61,32 @@ final class GitRepoRepoBlameForFileTest extends TestCase {
 			$this->options_git,
 			$this->options_git_repo_tests
 		);
-	
+
 		$this->options[ 'github-token' ] =
 			vipgoci_unittests_get_config_value(
 				'git-secrets',
 				'github-token',
 				true // Fetch from secrets file
 			);
+
+		$this->options['commit'] =
+			$this->options['commit-test-repo-blame-for-file-2'];
+
+		vipgoci_unittests_output_suppress();
+
+		$this->options['local-git-repo'] =
+			vipgoci_unittests_setup_git_repo(
+				$this->options
+			);
+
+		vipgoci_unittests_output_unsuppress();
 	}
 
+	/**
+	 * Tear down function, remove repository, clean up variables.
+	 *
+	 * @return void
+	 */
 	protected function tearDown(): void {
 		if ( false !== $this->options['local-git-repo'] ) {
 			vipgoci_unittests_remove_git_repo(
@@ -53,9 +100,14 @@ final class GitRepoRepoBlameForFileTest extends TestCase {
 	}
 
 	/**
+	 * Get 'git blame' for two non-empty files, check if returned
+	 * values are as expected.
+	 *
+	 * @return void
+	 *
 	 * @covers ::vipgoci_gitrepo_blame_for_file
 	 */
-	public function testGitRepoBlameForFile1() {
+	public function testGitRepoBlameForFiles() :void {
 		$options_test = vipgoci_unittests_options_test(
 			$this->options,
 			array( 'github-token', 'token' ),
@@ -65,18 +117,6 @@ final class GitRepoRepoBlameForFileTest extends TestCase {
 		if ( -1 === $options_test ) {
 			return;
 		}
-
-		$this->options['commit'] =
-			$this->options['commit-test-repo-blame-for-file-2'];
-
-		vipgoci_unittests_output_suppress();
-
-		$this->options['local-git-repo'] =
-			vipgoci_unittests_setup_git_repo(
-				$this->options
-			);
-
-	
 		if ( false === $this->options['local-git-repo'] ) {
 			$this->markTestSkipped(
 				'Could not set up git repository: ' .
@@ -87,6 +127,7 @@ final class GitRepoRepoBlameForFileTest extends TestCase {
 		$this->options['token'] =
 			$this->options['github-token'];
 
+		vipgoci_unittests_output_suppress();
 
 		$ret = vipgoci_gitrepo_blame_for_file(
 			$this->options['commit'],
@@ -319,6 +360,33 @@ final class GitRepoRepoBlameForFileTest extends TestCase {
 					'content'	=> 'Last line of file',
 				),
 			),
+			$ret
+		);
+	}
+
+	/**
+	 * Get 'git blame' for an empty file.
+	 *
+	 * @return void
+	 *
+	 * @covers ::vipgoci_gitrepo_blame_for_file
+	 */
+	public function testGitRepoBlameForEmptyFile() {
+		/*
+		 * Check empty file.
+		 */
+		vipgoci_unittests_output_suppress();
+
+		$ret = vipgoci_gitrepo_blame_for_file(
+			$this->options['commit'],
+			'file2.txt',
+			$this->options['local-git-repo']
+		);
+
+		vipgoci_unittests_output_unsuppress();
+
+		$this->assertSame(
+			array(),
 			$ret
 		);
 	}
