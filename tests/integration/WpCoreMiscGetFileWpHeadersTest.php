b57adfb02a8c31a388279794c1f998d8bd5ed602
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-07-11 18:49:00 +0000

    Requiring different file.

--- tests/integration/WpCoreMiscGetFileWpHeadersTest.php
+++ tests/integration/WpCoreMiscGetFileWpHeadersTest.php
@@ -24,8 +24,7 @@ final class WpCoreMiscGetFileWpHeadersTest extends TestCase {
 	 * @return void
 	 */
 	protected function setUp() :void {
-		require_once __DIR__ . '/../../defines.php';
-		require_once __DIR__ . '/../../wp-core-misc.php';
+		require_once __DIR__ . '/IncludesForTests.php';
 	}
 
 	/**
