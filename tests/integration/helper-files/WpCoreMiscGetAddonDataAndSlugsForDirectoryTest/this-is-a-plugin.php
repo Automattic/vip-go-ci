16030b7baf4f864c93777dd5c4e4e31f3730291e
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-07-11 21:36:06 +0000

    Adding example plugin header file and a non-plugin header file.

---
+++ tests/integration/helper-files/WpCoreMiscGetAddonDataAndSlugsForDirectoryTest/this-is-a-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package MyPackage
+ * @version 1.0.0
+ */
+/*
+ * Plugin Name: This is a plugin.
+ * Plugin URI: http://wordpress.org/test/my-other-package/
+ * Description: This is indeed <b>a plugin</b>..
+ * Author: Test author.
+ * Version: 15.1.0  
+ * Author URI: http://wordpress.org/author/test124
+ */
