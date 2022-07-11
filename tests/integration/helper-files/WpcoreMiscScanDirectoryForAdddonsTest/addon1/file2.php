24de304a67ab5c0a7e41739f913fe012c3b11651
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-07-11 14:52:25 +0000

    Switching from tar file to directory structure

---
+++ tests/integration/helper-files/WpcoreMiscScanDirectoryForAdddonsTest/addon1/file2.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package MyPackage
+ * @version 1.0.0
+ */
+/*
+ * Plugin Name: My Other Package 
+ * Plugin URI: http://wordpress.org/test/my-other-package/
+ * Description: My text.
+ * Author: Author Name
+ * Version: 1.1.0  
+ * Author URI: http://wordpress.org/author/test123
+ */
