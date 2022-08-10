2f06009aab7827693787e2937c66fd40e91dd0f4
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-08-10 15:50:22 +0000

    Add test for vipgoci_gitrepo_diffs_clean_extra_whitespace()

---
+++ tests/unit/GitRepoDiffsCleanExtraWhitespaceTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Test vipgoci_gitrepo_diffs_clean_extra_whitespace() function.
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+final class GitRepoDiffsCleanExtraWhitespaceTest extends TestCase {
+	/**
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
+	 */
+	protected function setUp() :void {
+		require_once __DIR__ . '/../../git-repo.php';
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_gitrepo_diffs_clean_extra_whitespace
+	 *
+	 * @return void
+	 */
+	public function testCleanExtraWhitespace(): void {
+		$this->assertSame(
+			array( "a\t" ),
+			vipgoci_gitrepo_diffs_clean_extra_whitespace(
+				array( "a\t")
+			)
+		);
+
+		$this->assertSame(
+			array( "a\t", "b\t", "c.php" ),
+			vipgoci_gitrepo_diffs_clean_extra_whitespace(
+				array( "a\t", "b\t", "c.php\t")
+			)
+		);
+	}
+}
