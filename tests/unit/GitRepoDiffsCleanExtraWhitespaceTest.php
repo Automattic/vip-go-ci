63fa810df0bb8335a9a9718be7759aa80fd8351f
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-08-10 15:51:50 +0000

    Breaking function up into two parts.

--- tests/unit/GitRepoDiffsCleanExtraWhitespaceTest.php
+++ tests/unit/GitRepoDiffsCleanExtraWhitespaceTest.php
@@ -34,14 +34,23 @@ final class GitRepoDiffsCleanExtraWhitespaceTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testCleanExtraWhitespace(): void {
+	public function testCleanExtraWhitespace1(): void {
 		$this->assertSame(
 			array( "a\t" ),
 			vipgoci_gitrepo_diffs_clean_extra_whitespace(
 				array( "a\t")
 			)
 		);
+	}
 
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_gitrepo_diffs_clean_extra_whitespace
+	 *
+	 * @return void
+	 */
+	public function testCleanExtraWhitespace2(): void {
 		$this->assertSame(
 			array( "a\t", "b\t", "c.php" ),
 			vipgoci_gitrepo_diffs_clean_extra_whitespace(
