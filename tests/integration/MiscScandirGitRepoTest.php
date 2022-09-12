ff49ce21a3d13b956c41925fff1ca7252547f72b
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-09-12 15:23:22 +0000

    Apply WP CS.

--- tests/integration/MiscScandirGitRepoTest.php
+++ tests/integration/MiscScandirGitRepoTest.php
@@ -1,23 +1,53 @@
 <?php
+/**
+ * Test vipgoci_scandir_git_repo().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
-require_once( __DIR__ . '/IncludesForTests.php' );
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MiscScandirGitRepoTest extends TestCase {
-	var $options_git = array(
-		'git-path'		=> null,
-		'github-repo-url'	=> null,
-		'repo-name'		=> null,
-		'repo-owner'		=> null,
+	/**
+	 * Git options.
+	 *
+	 * @var $options_git
+	 */
+	private array $options_git = array(
+		'git-path'        => null,
+		'github-repo-url' => null,
+		'repo-name'       => null,
+		'repo-owner'      => null,
 	);
 
-	var $git_repo_tests = array(
-		'commit-test-scandir-repo-test-1'	=> null,
-		'commit-test-scandir-repo-test-2'	=> null,
+	/**
+	 * Options for git repo tests.
+	 *
+	 * @var $options_git_repo_tests
+	 */
+	private array $git_repo_tests = array(
+		'commit-test-scandir-repo-test-1' => null,
+		'commit-test-scandir-repo-test-2' => null,
 	);
 
+	/**
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
+	 */
 	protected function setUp(): void {
+		require_once __DIR__ . '/IncludesForTests.php';
+
 		vipgoci_unittests_get_config_values(
 			'git',
 			$this->options_git
@@ -33,17 +63,20 @@ final class MiscScandirGitRepoTest extends TestCase {
 			$this->git_repo_tests
 		);
 
-		$this->options[ 'github-token' ] =
+		$this->options['github-token'] =
 			vipgoci_unittests_get_config_value(
 				'git-secrets',
 				'github-token',
-				true // Fetch from secrets file
+				true // Fetch from secrets file.
 			);
 
 		$this->options['token'] =
 			$this->options['github-token'];
 	}
 
+	/**
+	 * Clean up function, remove repository and unset variables.
+	 */
 	protected function tearDown(): void {
 		if ( false !== $this->options['local-git-repo'] ) {
 			vipgoci_unittests_remove_git_repo(
@@ -51,12 +84,14 @@ final class MiscScandirGitRepoTest extends TestCase {
 			);
 		}
 
-		$this->options_git = null;
-		$this->git_repo_tests = null;
-		$this->options = null;
+		unset( $this->options_git );
+		unset( $this->git_repo_tests );
+		unset( $this->options );
 	}
 
 	/**
+	 * Test function with subdirectories enabled and no filter.
+	 *
 	 * @covers ::vipgoci_scandir_git_repo
 	 */
 	public function testScandirRepoTestWithSubdirectoriesAndNoFilter() {
@@ -100,16 +135,18 @@ final class MiscScandirGitRepoTest extends TestCase {
 		$this->assertSame(
 			array(
 				'README.md',
- 				'myfile1.txt',	
+				'myfile1.txt',
 				'myfile2.txt',
 				'myfolder5/myotherfolder6/somefile2.txt',
-				'myfolder5/somefile1.txt'
+				'myfolder5/somefile1.txt',
 			),
 			$ret
 		);
 	}
 
 	/**
+	 * Test function with subdirectories enabled and filter applied.
+	 *
 	 * @covers ::vipgoci_scandir_git_repo
 	 */
 	public function testScandirRepoTestWithSubdirectoriesAndFilter() {
@@ -146,7 +183,7 @@ final class MiscScandirGitRepoTest extends TestCase {
 			$this->options['local-git-repo'],
 			true,
 			array(
-				'file_extensions'	=> array( 'md' )
+				'file_extensions' => array( 'md' ),
 			)
 		);
 
@@ -154,7 +191,7 @@ final class MiscScandirGitRepoTest extends TestCase {
 
 		$this->assertSame(
 			array(
-				'README.md'
+				'README.md',
 			),
 			$ret
 		);
