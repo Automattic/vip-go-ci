477cab4e5f1a42ea28b76db7c1ddd21e3da7533b
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-09-15 18:28:13 +0000

    Updating test, using assertSame()

--- tests/integration/WpCoreMiscGetAddonDataAndSlugsForDirectoryTest.php
+++ tests/integration/WpCoreMiscGetAddonDataAndSlugsForDirectoryTest.php
@@ -40,12 +40,30 @@ final class WpCoreMiscGetAddonDataAndSlugsForDirectoryTest extends TestCase {
 	private const KEY_PLUGIN_THIS_IS = 'vipgoci-addon-plugin-this-is-a-plugin.php';
 
 	/**
+	 * Variable for WPScan API scanning.
+	 *
+	 * @var $options_wpscan_api_scan
+	 */
+	private array $options_wpscan_api_scan = array(
+		'wpscan-pr-1-plugin-name'     => null,
+		'wpscan-pr-1-plugin-slug'     => null,
+		'wpscan-pr-1-plugin-dir'      => null,
+		'wpscan-pr-1-plugin-key'      => null,
+		'wpscan-pr-1-plugin-name-api' => null,
+	);
+
+	/**
 	 * Setup function. Require files.
 	 *
 	 * @return void
 	 */
 	protected function setUp() :void {
 		require_once __DIR__ . '/IncludesForTests.php';
+
+		vipgoci_unittests_get_config_values(
+			'wpscan-api-scan',
+			$this->options_wpscan_api_scan
+		);
 
 		$this->temp_dir = sys_get_temp_dir() .
 			'/directory_for_addons-' .
@@ -99,7 +117,7 @@ final class WpCoreMiscGetAddonDataAndSlugsForDirectoryTest extends TestCase {
 
 		if ( false === exec( $cp_cmd ) ) {
 			$this->markTestSkipped(
-				'Unable to extract tar file'
+				'Unable to copy files'
 			);
 
 			return;
@@ -115,31 +133,17 @@ final class WpCoreMiscGetAddonDataAndSlugsForDirectoryTest extends TestCase {
 		vipgoci_unittests_output_unsuppress();
 
 		/*
-		 * Ensure hello/hello.php is in results.
+		 * Verify a few values separately and remove before
+		 * main assertion.
 		 */
-		$this->assertNotEmpty(
-			$actual_results[ self::KEY_PLUGIN_HELLO ]
+
+		// For hello/hello.php.
+		$this->assertStringContainsString(
+			'WpCoreMiscGetAddonDataAndSlugsForDirectoryTest/hello/hello.php',
+			$actual_results[ self::KEY_PLUGIN_HELLO ]['file_name']
 		);
 
-		$this->assertSame(
-			'w.org/plugins/hello-dolly',
-			$actual_results[ self::KEY_PLUGIN_HELLO ]['id']
-		);
-
-		$this->assertSame(
-			'Hello Dolly',
-			$actual_results[ self::KEY_PLUGIN_HELLO ]['name']
-		);
-
-		$this->assertSame(
-			'hello-dolly',
-			$actual_results[ self::KEY_PLUGIN_HELLO ]['slug']
-		);
-
-		$this->assertSame(
-			'hello/hello.php',
-			$actual_results[ self::KEY_PLUGIN_HELLO ]['plugin']
-		);
+		unset( $actual_results[ self::KEY_PLUGIN_HELLO ]['file_name'] );
 
 		$this->assertTrue(
 			version_compare(
@@ -149,67 +153,84 @@ final class WpCoreMiscGetAddonDataAndSlugsForDirectoryTest extends TestCase {
 			)
 		);
 
-		$this->assertStringContainsString(
-			'/plugins/hello-dolly',
-			$actual_results[ self::KEY_PLUGIN_HELLO ]['url']
-		);
+		unset( $actual_results[ self::KEY_PLUGIN_HELLO ]['new_version'] );
 
 		$this->assertStringContainsString(
 			'/hello-dolly.',
 			$actual_results[ self::KEY_PLUGIN_HELLO ]['package']
 		);
 
-		/*
-		 * Ensure this-is-a-plugin.php is in results.
-		 */
+		unset( $actual_results[ self::KEY_PLUGIN_HELLO ]['package'] );
 
-		$this->assertNotEmpty(
-			$actual_results[ self::KEY_PLUGIN_THIS_IS ]
+		// For this-is-a-plugin.php.
+		foreach (
+			array( 'id', 'slug', 'plugin', 'new_version', 'url', 'package' ) as $field_name
+		) {
+			$this->assertFalse(
+				isset( $actual_results[ self::KEY_PLUGIN_THIS_IS ][ $field_name ] )
+			);
+		}
+
+		$this->assertStringContainsString(
+			'WpCoreMiscGetAddonDataAndSlugsForDirectoryTest/this-is-a-plugin.php',
+			$actual_results[ self::KEY_PLUGIN_THIS_IS ]['file_name']
 		);
 
-		$this->assertFalse(
-			isset( $actual_results[ self::KEY_PLUGIN_THIS_IS ]['id'] )
-		);
-
-		$this->assertSame(
-			'This is a plugin.',
-			$actual_results[ self::KEY_PLUGIN_THIS_IS ]['name']
-		);
-
-		$this->assertFalse(
-			isset( $actual_results[ self::KEY_PLUGIN_THIS_IS ]['slug'] )
-		);
-
-		$this->assertFalse(
-			isset( $actual_results[ self::KEY_PLUGIN_THIS_IS ]['plugin'] )
-		);
-
-		$this->assertSame(
-			'15.1.0',
-			$actual_results[ self::KEY_PLUGIN_THIS_IS ]['version_detected']
-		);
-
-		$this->assertFalse(
-			isset( $actual_results[ self::KEY_PLUGIN_THIS_IS ]['new_version'] )
-		);
-
-		$this->assertFalse(
-			isset( $actual_results[ self::KEY_PLUGIN_THIS_IS ]['url'] )
-		);
-
-		$this->assertFalse(
-			isset( $actual_results[ self::KEY_PLUGIN_THIS_IS ]['package'] )
-		);
+		unset( $actual_results[ self::KEY_PLUGIN_THIS_IS ]['file_name'] );
 
 		/*
-		 * Ensure only the two plugins are in results.
+		 * Perform main assertion
 		 */
 		$this->assertSame(
 			array(
-				self::KEY_PLUGIN_HELLO,
-				self::KEY_PLUGIN_THIS_IS,
+				self::KEY_PLUGIN_HELLO   => array(
+					'type'             => 'vipgoci-addon-plugin',
+					'addon_headers'    => array(
+						'Name'        => 'Hello Dolly',
+						'PluginURI'   => 'http://wordpress.org/plugins/hello-dolly/',
+						'Version'     => '1.7.2',
+						'Description' => 'This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.',
+						'Author'      => 'Matt Mullenweg',
+						'AuthorURI'   => 'http://ma.tt/',
+						'TextDomain'  => '',
+						'DomainPath'  => '',
+						'Network'     => '',
+						'RequiresWP'  => '',
+						'RequiresPHP' => '',
+						'UpdateURI'   => '',
+						'Title'       => 'Hello Dolly',
+						'AuthorName'  => 'Matt Mullenweg',
+					),
+					'name'             => $this->options_wpscan_api_scan['wpscan-pr-1-plugin-name'],
+					'version_detected' => '1.7.2',
+					'id'               => 'w.org/plugins/' . $this->options_wpscan_api_scan['wpscan-pr-1-plugin-slug'],
+					'slug'             => $this->options_wpscan_api_scan['wpscan-pr-1-plugin-slug'],
+					'plugin'           => $this->options_wpscan_api_scan['wpscan-pr-1-plugin-name-api'],
+					'url'              => 'https://wordpress.org/plugins/' . $this->options_wpscan_api_scan['wpscan-pr-1-plugin-slug'] . '/',
+				),
+				self::KEY_PLUGIN_THIS_IS => array(
+					'type'             => 'vipgoci-addon-plugin',
+					'addon_headers'    => array(
+						'Name'        => 'This is a plugin.',
+						'PluginURI'   => 'http://wordpress.org/test/my-other-package/',
+						'Version'     => '15.1.0',
+						'Description' => 'This is indeed <b>a plugin</b>..',
+						'Author'      => 'Test author.',
+						'AuthorURI'   => 'http://wordpress.org/author/test124',
+						'TextDomain'  => '',
+						'DomainPath'  => '',
+						'Network'     => '',
+						'RequiresWP'  => '',
+						'RequiresPHP' => '',
+						'UpdateURI'   => '',
+						'Title'       => 'This is a plugin.',
+						'AuthorName'  => 'Test author.',
+					),
+					'name'             => 'This is a plugin.',
+					'version_detected' => '15.1.0',
+				),
 			),
-			array_keys( $actual_results )
+			$actual_results
 		);
 	}
 
@@ -237,7 +258,7 @@ final class WpCoreMiscGetAddonDataAndSlugsForDirectoryTest extends TestCase {
 
 		if ( false === exec( $cp_cmd ) ) {
 			$this->markTestSkipped(
-				'Unable to extract tar file'
+				'Unable to copy files'
 			);
 
 			return;
