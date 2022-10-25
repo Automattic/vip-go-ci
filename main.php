284e2d4f31ea15318480c3bcb7d42ac56b751241
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-10-25 14:41:47 +0000

    Fix indenting.

--- main.php
+++ main.php
@@ -1899,7 +1899,7 @@ function vipgoci_run_cleanup_send_pixel_api(
 					'http_api_request_put',
 					'http_api_request_delete',
 					'http_api_request_failure',
-          'http_api_request_limit_reached',
+					'http_api_request_limit_reached',
 					'github_pr_approval',
 					'github_pr_non_approval',
 					'github_pr_lint_issues',
@@ -1914,8 +1914,8 @@ function vipgoci_run_cleanup_send_pixel_api(
 					$options['repo-owner'] . '-' .
 					$options['repo-name']
 				=> array(
-          'http_api_request_failure',
-          'http_api_request_limit_reached',
+					'http_api_request_failure',
+					'http_api_request_limit_reached',
 					'github_pr_approval',
 					'github_pr_non_approval',
 					'github_pr_files_linted',
