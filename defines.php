4c38674a2afe9fdadb53925fd6f9db068fab915d
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-10-26 16:16:31 +0000

    Adding constants.

--- defines.php
+++ defines.php
@@ -81,9 +81,10 @@ define( 'VIPGOCI_ISSUE_TYPE_WARNING', 'warning' );
 define( 'VIPGOCI_ISSUE_TYPE_ERROR', 'error' );
 
 /*
- * Define auto-approval types
+ * Defines for auto-approvals.
  */
 define( 'VIPGOCI_APPROVAL_AUTOAPPROVE', 'auto-approval' );
+define( 'VIPGOCI_APPROVAL_AUTOAPPROVE_NON_FUNCTIONAL_CHANGES_FILE_EXTENSIONS_DEFAULT', array( 'php' ) );
 
 /*
  * Defines for vipgoci_runtime_measure() function.
@@ -204,8 +205,10 @@ define(
 );
 
 /*
- * Messages related to PHP linting.
+ * Defines related to PHP linting.
  */
+define( 'VIPGOCI_LINT_FILE_EXTENSIONS_DEFAULT', array( 'php' ) );
+
 define(
 	'VIPGOCI_LINT_REPORT_START',
 	'%1$s has identified PHP syntax errors during automated linting. ' .
@@ -304,6 +307,8 @@ define( 'VIPGOCI_WORDPRESS_ORG_API_BASE_URL', 'https://api.wordpress.org' );
  */
 define( 'VIPGOCI_WPSCAN_BASE_URL', 'https://wpscan.com' );
 define( 'VIPGOCI_WPSCAN_API_BASE_URL', VIPGOCI_WPSCAN_BASE_URL . '/api/v3' );
+
+define( 'VIPGOCI_WPSCAN_FILE_EXTENSIONS_DEFAULT', array( 'php', 'css' ) );
 
 define( 'VIPGOCI_WPSCAN_UPDATEURI_WP_ORG_URLS', array( 'w.org', 'wordpress.org' ) );
 
