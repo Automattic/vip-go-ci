407a9c74be618557b27d777327cbdc8c979c0503
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-10-27 10:55:59 +0000

    Add --autoapprove-php-nonfunctional-changes-file-extensions option

--- tests/unit/MainRunInitOptionsAutoapproveTest.php
+++ tests/unit/MainRunInitOptionsAutoapproveTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
+require_once __DIR__ . '/../../defines.php';
 require_once __DIR__ . '/../../main.php';
 require_once __DIR__ . '/../../options.php';
 require_once __DIR__ . '/../../misc.php';
@@ -42,6 +43,7 @@ final class MainRunInitOptionsAutoapproveTest extends TestCase {
 		$this->options = array(
 			'autoapprove'                           => null,
 			'autoapprove-php-nonfunctional-changes' => null,
+			'autoapprove-php-nonfunctional-changes-file-extensions' => null,
 			'autoapprove-filetypes'                 => 'txt,gif,png',
 			'autoapprove-label'                     => 'MyText1',
 		);
@@ -54,7 +56,8 @@ final class MainRunInitOptionsAutoapproveTest extends TestCase {
 			array(
 				'autoapprove'                           => false,
 				'autoapprove-php-nonfunctional-changes' => false,
-				'autoapprove-filetypes'                 => array( 'txt', 'gif', 'png' ),
+				'autoapprove-php-nonfunctional-changes-file-extensions' => array( 'php' ),
+				'autoapprove-filetypes'                 => array(),
 				'autoapprove-label'                     => 'MyText1',
 			),
 			$this->options
@@ -71,8 +74,12 @@ final class MainRunInitOptionsAutoapproveTest extends TestCase {
 		$this->options = array(
 			'autoapprove'                           => 'true',
 			'autoapprove-php-nonfunctional-changes' => 'true',
+			'autoapprove-php-nonfunctional-changes-file-extensions' => 'php,inc',
 			'autoapprove-filetypes'                 => 'txt,gif,png,pdf',
 			'autoapprove-label'                     => 'MyText2',
+			'lint-file-extensions'                  => array( 'php' ),
+			'phpcs-file-extensions'                 => array( 'php', 'js', 'twig' ),
+			'svg-file-extensions'                   => array( 'svg' ),
 		);
 
 		vipgoci_run_init_options_autoapprove(
@@ -83,8 +90,12 @@ final class MainRunInitOptionsAutoapproveTest extends TestCase {
 			array(
 				'autoapprove'                           => true,
 				'autoapprove-php-nonfunctional-changes' => true,
+				'autoapprove-php-nonfunctional-changes-file-extensions' => array( 'php', 'inc' ),
 				'autoapprove-filetypes'                 => array( 'txt', 'gif', 'png', 'pdf' ),
 				'autoapprove-label'                     => 'MyText2',
+				'lint-file-extensions'                  => array( 'php' ),
+				'phpcs-file-extensions'                 => array( 'php', 'js', 'twig' ),
+				'svg-file-extensions'                   => array( 'svg' ),
 			),
 			$this->options
 		);
