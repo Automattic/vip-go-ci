1e15d616120f2dff578e47bb02a2bf3610cb9e4b
Gudmundur Haraldsson <gudmundur.haraldsson@automattic.com>
2022-10-27 11:00:10 +0000

    Restructure test a bit

--- tests/unit/MainRunInitOptionsAutoapproveTest.php
+++ tests/unit/MainRunInitOptionsAutoapproveTest.php
@@ -1,13 +1,13 @@
 <?php
+/**
+ * Test vipgoci_run_init_options_autoapprove() function.
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
-
-require_once __DIR__ . '/../../defines.php';
-require_once __DIR__ . '/../../main.php';
-require_once __DIR__ . '/../../options.php';
-require_once __DIR__ . '/../../misc.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -20,14 +20,23 @@ use PHPUnit\Framework\TestCase;
  */
 final class MainRunInitOptionsAutoapproveTest extends TestCase {
 	/**
-	 * Set up variable.
+	 * Require files, set up variable.
+	 *
+	 * @return void
 	 */
 	protected function setUp() :void {
+		require_once __DIR__ . '/../../defines.php';
+		require_once __DIR__ . '/../../main.php';
+		require_once __DIR__ . '/../../options.php';
+		require_once __DIR__ . '/../../misc.php';
+
 		$this->options = array();
 	}
 
 	/**
 	 * Clear variable.
+	 *
+	 * @return void
 	 */
 	protected function tearDown() :void {
 		unset( $this->options );
@@ -38,6 +47,8 @@ final class MainRunInitOptionsAutoapproveTest extends TestCase {
 	 * correctly provided.
 	 *
 	 * @covers ::vipgoci_run_init_options_autoapprove
+	 *
+	 * @return void
 	 */
 	public function testRunInitOptionsAutoapproveDefault() :void {
 		$this->options = array(
