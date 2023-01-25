e5e77f1f727fdb27322ccf7aae8e1fc348146f8b
gdh <gudmundur.haraldsson@automattic.com>
2023-01-25 18:14:03 +0000

    Adding comments, --help message, etc.

--- github-utils/create-pull-requests.php
+++ github-utils/create-pull-requests.php
@@ -14,6 +14,10 @@ define( 'VIPGOCI_INCLUDED', true );
 /**
  * Ask the GitHub HTTP API to create pull requests specified.
  *
+ * @param array $options         Options array for the program.
+ * @param array $pr_items        Pull requests to create.
+ * @param array $pr_items_failed Reference to array for pull requests not created.
+ *
  * @return void
  */
 function cpr_create_pull_requests(
@@ -58,8 +62,18 @@ function cpr_create_pull_requests(
  * @return void
  */
 function cpr_main() {
+	global $argv;
+
 	require_once __DIR__ . '/../requires.php';
 
+	/*
+	 * Configure PHP error reporting.
+	 */
+	vipgoci_set_php_error_reporting();
+
+	/*
+	 * Recognized options, get options.
+	 */
 	$options_recognized = array(
 		'repo-owner:',
 		'repo-name:',
@@ -73,10 +87,16 @@ function cpr_main() {
 		$options_recognized
 	);
 
-	if ( empty( $options['env-options'] ) ) {
-		echo 'Missing --env-options' . PHP_EOL;
-		exit( 1 );
-	}
+	/*
+	 * Options to remove of any sensitive
+	 * detail when cleaned later.
+	 */
+	vipgoci_options_sensitive_clean(
+		null,
+		array(
+			'github-token',
+		)
+	);
 
 	vipgoci_option_array_handle(
 		$options,
@@ -93,13 +113,35 @@ function cpr_main() {
 	);
 
 	if (
-		( empty( $options['repo-owner'] ) ) ||
-		( empty( $options['repo-name'] ) ) ||
-		( empty( $options['github-token'] ) ) ||
-		( empty( $options['pull-requests'] ) )
+		( ! isset(
+			$options['repo-owner'],
+			$options['repo-name'],
+			$options['github-token'],
+			$options['pull-requests'],
+		) )
+		||
+		(
+			isset( $options['help'] )
+		)
 	) {
-		echo 'Missing options' . PHP_EOL;
-		exit( 1 );
+		print 'Usage: ' . $argv[0] . PHP_EOL .
+			PHP_EOL .
+			"\t" . '--repo-owner=STRING            Specify repository owner, can be an organization.' . PHP_EOL .
+			"\t" . '--repo-name=STRING             Specify name of the repository.' . PHP_EOL .
+			"\t" . '--github-token=STRING          The access-token to use to communicate with GitHub.' . PHP_EOL .
+			PHP_EOL .
+			"\t" . '--pull-requests=STRING         Specify pull requests to create. Expects JSON format. For example:' . PHP_EOL .
+			"\t" . '                               [{"title":"test branch","body":"Test pull request","head":"testing1","base":"main"},{...}]' . PHP_EOL .
+			PHP_EOL .
+			"\t" . '--env-options=STRING           Specifies configuration options to be read from environmental' . PHP_EOL .
+			"\t" . '                               variables -- any variable can be specified. For example:' . PHP_EOL .
+			"\t" . '                               --env-options="repo-owner=U_ROWNER,output=U_FOUTPUT"' . PHP_EOL .
+			PHP_EOL .
+			"\t" . '--help                         Prints this message.' . PHP_EOL .
+			PHP_EOL .
+			'All options, except --help, are mandatory.' . PHP_EOL;
+
+		exit( VIPGOCI_EXIT_USAGE_ERROR );
 	}
 
 	$options['pull-requests'] = json_decode(
