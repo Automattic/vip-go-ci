11b27d38ceff4418820695de103a3e807a2e59f9
gdh <gudmundur.haraldsson@automattic.com>
2023-02-06 19:56:31 +0000

    Apply WP CS

--- tests/unit/GitHubMiscMarkdownCommentAddPagebreakTest.php
+++ tests/unit/GitHubMiscMarkdownCommentAddPagebreakTest.php
@@ -1,18 +1,40 @@
 <?php
+/**
+ * Test vipgoci_markdown_comment_add_pagebreak().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../misc.php' );
-
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class GitHubMiscMarkdownCommentAddPagebreakTest extends TestCase {
 	/**
-	 * @covers ::vipgoci_markdown_comment_add_pagebreak
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
-	public function testPageBreak1() {
+	protected function setUp() :void {
+		require_once __DIR__ . './../../github-misc.php';
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_markdown_comment_add_pagebreak
+	 *
+	 * @return void
+	 */
+	public function testPageBreak1() :void {
 		$mycomment = 'Here is my text. ' . "\n\r";
 
 		vipgoci_markdown_comment_add_pagebreak(
@@ -22,7 +44,7 @@ final class GitHubMiscMarkdownCommentAddPagebreakTest extends TestCase {
 
 		$this->assertSame(
 			'Here is my text. ' . "\n\r" . '***' . "\n\r",
-			$mycomment 
+			$mycomment
 		);
 	}
 }
