117e4e4bcb43ddc20d8806efd20f4fe1b84e6d34
gdh <gudmundur.haraldsson@automattic.com>
2023-02-06 19:06:13 +0000

    Apply WP CS

--- tests/unit/A00StatsCountersTest.php
+++ tests/unit/A00StatsCountersTest.php
@@ -1,18 +1,17 @@
 <?php
+/**
+ * Test vipgoci_counter_report().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../defines.php' );
-require_once( __DIR__ . './../../statistics.php' );
-
 use PHPUnit\Framework\TestCase;
 
-// phpcs:disable PSR1.Files.SideEffects
-
 /**
- *
  * Test counter functionality.
  *
  * @runTestsInSeparateProcesses
@@ -20,9 +19,23 @@ use PHPUnit\Framework\TestCase;
  */
 final class A00StatsCountersTest extends TestCase {
 	/**
-	 * @covers ::vipgoci_counter_report
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
-	function testCounterReport1() {
+	protected function setUp() :void {
+		require_once __DIR__ . './../../defines.php';
+		require_once __DIR__ . './../../statistics.php';
+	}
+
+	/**
+	 * Test with invalid parameters.
+	 *
+	 * @covers ::vipgoci_counter_report
+	 *
+	 * @return void
+	 */
+	public function testCounterReport1() :void {
 		$this->assertSame(
 			vipgoci_counter_report(
 				'illegalaction',
@@ -43,9 +56,13 @@ final class A00StatsCountersTest extends TestCase {
 	}
 
 	/**
+	 * Test with valid parameters.
+	 *
 	 * @covers ::vipgoci_counter_report
+	 *
+	 * @return void
 	 */
-	function testCounterReport2() {
+	public function testCounterReport2() :void {
 		$this->assertSame(
 			true,
 			vipgoci_counter_report(
@@ -74,27 +91,29 @@ final class A00StatsCountersTest extends TestCase {
 		);
 	}
 
-
-	/*
+	/**
+	 * Test common usage of the function.
+	 *
 	 * @covers ::vipgoci_counter_update_with_issues_found
+	 *
+	 * @return void
 	 */
-	function testCounterUpdateWithIssuesFound1() {
+	public function testCounterUpdateWithIssuesFound1() :void {
 		$results = array(
 			'stats' => array(
 				'unique_issue' => array(
 					120 => array(
-						'errors' => 1,
+						'errors'   => 1,
 						'warnings' => 1,
 					),
 
 					121 => array(
-						'errors' => 2,
+						'errors'   => 2,
 						'warnings' => 1,
 					),
-				)
-			)
+				),
+			),
 		);
-
 
 		vipgoci_counter_update_with_issues_found(
 			$results
@@ -104,16 +123,14 @@ final class A00StatsCountersTest extends TestCase {
 			VIPGOCI_COUNTERS_DUMP
 		);
 
-
 		unset( $report['mycounter2'] );
 
-	
 		$this->assertSame(
 			array(
 				'github_pr_unique_issue_issues' => 3,
 			),
 			$report
-		);	
+		);
 	}
 }
 
