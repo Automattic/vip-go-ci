74f02e4f48c862ee6936f57c86cac5c47b69725f
gdh <gudmundur.haraldsson@automattic.com>
2023-02-06 19:48:32 +0000

    Apply WP CS

--- tests/unit/GitHubMiscGitHubPrRemoveDraftsTest.php
+++ tests/unit/GitHubMiscGitHubPrRemoveDraftsTest.php
@@ -1,53 +1,75 @@
 <?php
+/**
+ * Test vipgoci_github_pr_remove_drafts().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../github-misc.php' );
-
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class GitHubMiscGitHubPrRemoveDraftsTest extends TestCase {
 	/**
-	 * @covers ::vipgoci_github_pr_remove_drafts
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
-	public function testRemoveDraftPrs() {
+	protected function setUp() :void {
+		require_once __DIR__ . './../../github-misc.php';
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_github_pr_remove_drafts
+	 *
+	 * @return void
+	 */
+	public function testRemoveDraftPrs() :void {
 		$prs_array = array(
 			(object) array(
-				'url'		=> 'https://myapi.mydomain.is',
-				'id'		=> 123,
-				'node_id'	=> 'testing',
-				'state'		=> 'open',
-				'draft'		=> true
+				'url'     => 'https://myapi.mydomain.is',
+				'id'      => 123,
+				'node_id' => 'testing',
+				'state'   => 'open',
+				'draft'   => true,
 			),
 
 			(object) array(
- 				'url'		=> 'https://myapi2.mydomain.is',
-				'id'		=> 999,
-				'node_id'	=> 'testing2',
-				'state'		=> 'open',
-				'draft'		=> false
-			)
+				'url'     => 'https://myapi2.mydomain.is',
+				'id'      => 999,
+				'node_id' => 'testing2',
+				'state'   => 'open',
+				'draft'   => false,
+			),
 		);
 
 		$prs_array = vipgoci_github_pr_remove_drafts(
 			$prs_array
 		);
 
-		if ( isset( $prs_array[ 1 ] ) ) {
-			$prs_array[ 1 ] = (array) $prs_array[ 1 ];
+		if ( isset( $prs_array[1] ) ) {
+			$prs_array[1] = (array) $prs_array[1];
 		}
 
 		$this->assertSame(
 			array(
 				1 => array(
-	 				'url'		=> 'https://myapi2.mydomain.is',
-					'id'		=> 999,
-					'node_id'	=> 'testing2',
-					'state'		=> 'open',
-					'draft'		=> false
-				)
+					'url'     => 'https://myapi2.mydomain.is',
+					'id'      => 999,
+					'node_id' => 'testing2',
+					'state'   => 'open',
+					'draft'   => false,
+				),
 			),
 			$prs_array
 		);
