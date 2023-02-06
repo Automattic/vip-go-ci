76c7d86b813cf4f804a0b7e43db7c1918eb677df
gdh <gudmundur.haraldsson@automattic.com>
2023-02-06 20:50:08 +0000

    Apply WP CS

--- tests/unit/MainRunInitOptionsDebugTest.php
+++ tests/unit/MainRunInitOptionsDebugTest.php
@@ -1,11 +1,13 @@
 <?php
+/**
+ * Test vipgoci_run_init_options_debug().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
-
-require_once __DIR__ . '/../../main.php';
-require_once __DIR__ . '/../../options.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -18,8 +20,13 @@ use PHPUnit\Framework\TestCase;
 final class MainRunInitOptionsDebugTest extends TestCase {
 	/**
 	 * Set up variables.
+	 *
+	 * @return void
 	 */
 	protected function setUp() :void {
+		require_once __DIR__ . '/../../main.php';
+		require_once __DIR__ . '/../../options.php';
+
 		$this->options = array(
 			'debug-level' => '1',
 		);
@@ -31,6 +38,8 @@ final class MainRunInitOptionsDebugTest extends TestCase {
 
 	/**
 	 * Clear variables.
+	 *
+	 * @return void
 	 */
 	protected function tearDown() :void {
 		global $vipgoci_debug_level;
@@ -44,6 +53,8 @@ final class MainRunInitOptionsDebugTest extends TestCase {
 	 * Test if debug level is set correctly.
 	 *
 	 * @covers ::vipgoci_run_init_options_debug
+	 *
+	 * @return void
 	 */
 	public function testRunInitOptionsDebugDefault() :void {
 		vipgoci_run_init_options_debug(
