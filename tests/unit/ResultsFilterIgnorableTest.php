63c72743098d1da601dfa730fd4e7f5e2cd0bb52
gdh <gudmundur.haraldsson@automattic.com>
2023-02-06 20:29:52 +0000

    Applying WP CS

--- tests/unit/ResultsFilterIgnorableTest.php
+++ tests/unit/ResultsFilterIgnorableTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Test vipgoci_results_filter_ignorable().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
@@ -15,6 +20,8 @@ use PHPUnit\Framework\TestCase;
 final class ResultsFilterIgnorableTest extends TestCase {
 	/**
 	 * Require files.
+	 *
+	 * @return void
 	 */
 	protected function setUp() :void {
 		require_once __DIR__ . '/../../results.php';
