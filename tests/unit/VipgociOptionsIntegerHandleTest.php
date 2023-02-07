167355ac366d588c59e018b2bb8c2012833dbc83
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 15:34:48 +0000

    Apply WP CS

--- tests/unit/VipgociOptionsIntegerHandleTest.php
+++ tests/unit/VipgociOptionsIntegerHandleTest.php
@@ -1,20 +1,41 @@
 <?php
+/**
+ * Test function vipgoci_option_integer_handle().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../options.php' );
-
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class VipgociOptionsIntegerHandleTest extends TestCase {
 	/**
-	 * @covers ::vipgoci_option_integer_handle
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
-	public function testOptionsIntegerHandle1() {
-		$options = array(
-		);
+	protected function setUp() :void {
+		require_once __DIR__ . './../../options.php';
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_option_integer_handle
+	 *
+	 * @return void
+	 */
+	public function testOptionsIntegerHandle1() :void {
+		$options = array();
 
 		vipgoci_option_integer_handle(
 			$options,
@@ -24,7 +45,7 @@ final class VipgociOptionsIntegerHandleTest extends TestCase {
 
 		$this->assertSame(
 			array(
-				'mytestoption'	=> 5
+				'mytestoption' => 5,
 			),
 			$options
 		);
