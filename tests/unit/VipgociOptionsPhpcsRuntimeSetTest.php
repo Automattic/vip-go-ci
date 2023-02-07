946a1fd3e75a0ee232f04ed0c9f95de7f339455b
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 15:38:06 +0000

    Apply WP CS

--- tests/unit/VipgociOptionsPhpcsRuntimeSetTest.php
+++ tests/unit/VipgociOptionsPhpcsRuntimeSetTest.php
@@ -1,25 +1,47 @@
 <?php
+/**
+ * Test function vipgoci_option_phpcs_runtime_set().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../options.php' );
-
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class VipgociOptionsPhpcsRuntimeSetTest extends TestCase {
 	/**
-	 * @covers ::vipgoci_option_phpcs_runtime_set
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
-	public function testOptionsPhpcsRuntimeSet1() {
+	protected function setUp() :void {
+		require_once __DIR__ . './../../options.php';
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_option_phpcs_runtime_set
+	 *
+	 * @return void
+	 */
+	public function testOptionsPhpcsRuntimeSet1() :void {
 		$this->options = array(
 			'myphpcsruntimeoption' => 'testVersion 7.4-,allowUnusedVariablesBeforeRequire true,allowUndefinedVariablesInFileScope false',
-			'other-option1' => '123 456',
-			'other-option2' => array(
+			'other-option1'        => '123 456',
+			'other-option2'        => array(
 				'1',
 				'2',
-			)
+			),
 		);
 
 		vipgoci_option_phpcs_runtime_set(
@@ -32,7 +54,7 @@ final class VipgociOptionsPhpcsRuntimeSetTest extends TestCase {
 				'myphpcsruntimeoption' => array(
 					array(
 						'testVersion',
-						'7.4-'
+						'7.4-',
 					),
 					array(
 						'allowUnusedVariablesBeforeRequire',
@@ -40,20 +62,18 @@ final class VipgociOptionsPhpcsRuntimeSetTest extends TestCase {
 					),
 					array(
 						'allowUndefinedVariablesInFileScope',
-						'false'
+						'false',
 					),
 				),
-				'other-option1' => '123 456',
-				'other-option2' => array(
+				'other-option1'        => '123 456',
+				'other-option2'        => array(
 					'1',
 					'2',
-				)
+				),
 			),
 			$this->options
 		);
 
-		unset(
-			$this->options
-		);
+		unset( $this->options );
 	}
 }
