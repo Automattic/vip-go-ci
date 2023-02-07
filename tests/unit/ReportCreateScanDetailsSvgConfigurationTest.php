f83c584ed0af7577f8a9067b30babaf929232b2f
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 16:26:36 +0000

    Apply WP CS

--- tests/unit/ReportCreateScanDetailsSvgConfigurationTest.php
+++ tests/unit/ReportCreateScanDetailsSvgConfigurationTest.php
@@ -21,6 +21,8 @@ use PHPUnit\Framework\TestCase;
 final class ReportCreateScanDetailsSvgConfigurationTest extends TestCase {
 	/**
 	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
 	protected function setUp() :void {
 		require_once __DIR__ . '/../../reports.php';
@@ -33,6 +35,8 @@ final class ReportCreateScanDetailsSvgConfigurationTest extends TestCase {
 
 	/**
 	 * Clean up options variable.
+	 *
+	 * @return void
 	 */
 	protected function tearDown() :void {
 		unset( $this->options );
@@ -42,6 +46,8 @@ final class ReportCreateScanDetailsSvgConfigurationTest extends TestCase {
 	 * Test function with most reporting disabled.
 	 *
 	 * @covers ::vipgoci_report_create_scan_details_svg_configuration
+	 *
+	 * @return void
 	 */
 	public function testCreateDetails1(): void {
 		$this->options['svg-checks'] = false;
@@ -71,6 +77,8 @@ final class ReportCreateScanDetailsSvgConfigurationTest extends TestCase {
 	 * Test function with reporting enabled.
 	 *
 	 * @covers ::vipgoci_report_create_scan_details_svg_configuration
+	 *
+	 * @return void
 	 */
 	public function testCreateDetails2(): void {
 		$this->options['svg-checks'] = true;
