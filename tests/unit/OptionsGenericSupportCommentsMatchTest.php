84cba9212f1c1c36f375f239771c4420f66f26d4
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 14:18:18 +0000

    Apply WP CS

--- tests/unit/OptionsGenericSupportCommentsMatchTest.php
+++ tests/unit/OptionsGenericSupportCommentsMatchTest.php
@@ -1,27 +1,52 @@
 <?php
+/**
+ * Test function vipgoci_option_generic_support_comments_match().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../misc.php' );
-require_once( __DIR__ . './../../options.php' );
-
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class OptionsGenericSupportCommentsMatchTest extends TestCase {
-	public function setUp(): void {
+	/**
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
+	 */
+	protected function setUp() :void {
+		require_once __DIR__ . './../../misc.php';
+		require_once __DIR__ . './../../options.php';
+
 		$this->options = array();
-	}
-	
-	public function tearDown(): void {
-		$this->options = null;
 	}
 
 	/**
-	 * @covers ::vipgoci_option_generic_support_comments_match
+	 * Clean up function.
+	 *
+	 * @return void
 	 */
-	public function testOptionGenericSupportCommentsMatch () {
+	public function tearDown() :void {
+		unset( $this->options );
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_option_generic_support_comments_match
+	 *
+	 * @return void
+	 */
+	public function testOptionGenericSupportCommentsMatch() :void {
 		$this->options['myoption1'] =
 			'1:key1=value1,key2=value2,key3=value3,key4=value4a,key4=value4b|||2:key1=value1,key10=value10,key20=value20a,key20=value20b,key30=value30';
 
@@ -40,7 +65,7 @@ final class OptionsGenericSupportCommentsMatchTest extends TestCase {
 				),
 
 				'2' => array(
-					'key1' => array( 'value1' ),
+					'key1'  => array( 'value1' ),
 					'key10' => array( 'value10' ),
 					'key20' => array( 'value20a', 'value20b' ),
 					'key30' => array( 'value30' ),
