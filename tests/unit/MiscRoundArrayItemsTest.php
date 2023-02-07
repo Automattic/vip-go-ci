9766fd860f05438fb5e5ad96a73a15e7bc1b4bfc
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 14:07:01 +0000

    Apply WP CS

--- tests/unit/MiscRoundArrayItemsTest.php
+++ tests/unit/MiscRoundArrayItemsTest.php
@@ -1,25 +1,47 @@
 <?php
+/**
+ * Test function vipgoci_round_array_items().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../misc.php' );
-
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class MiscRoundArrayItemsTest extends TestCase {
 	/**
-	 * @covers ::vipgoci_round_array_items
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
-	public function testRoundArrayItems() {
+	protected function setUp() :void {
+		require_once __DIR__ . './../../misc.php';
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_round_array_items
+	 *
+	 * @return void
+	 */
+	public function testRoundArrayItems() :void {
 		$org_array = array(
-			'test1'	=> 10.333330,
-			'test2'	=> 0.034444444,
-			'test3'	=> 3.359999999,
-			'test4'	=> 5.0000003,
-			'test5'	=> 7.377777777,
-			'test6'	=> 5.00000001,
+			'test1' => 10.333330,
+			'test2' => 0.034444444,
+			'test3' => 3.359999999,
+			'test4' => 5.0000003,
+			'test5' => 7.377777777,
+			'test6' => 5.00000001,
 		);
 
 		$res_array = vipgoci_round_array_items(
@@ -29,12 +51,12 @@ final class MiscRoundArrayItemsTest extends TestCase {
 		);
 
 		$expected_array = array(
-			'test1'	=> 10.33,
-			'test2'	=> 0.03,
-			'test3'	=> 3.36,
-			'test4'	=> 5.00,
-			'test5'	=> 7.38,
-			'test6'	=> 5.0,
+			'test1' => 10.33,
+			'test2' => 0.03,
+			'test3' => 3.36,
+			'test4' => 5.00,
+			'test5' => 7.38,
+			'test6' => 5.0,
 		);
 
 		$this->assertSame(
