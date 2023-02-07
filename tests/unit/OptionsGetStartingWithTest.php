cb6ecae2c0fe358d83ba5c9e780ae1dadf44d519
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 14:44:54 +0000

    Apply WP CS

--- tests/unit/OptionsGetStartingWithTest.php
+++ tests/unit/OptionsGetStartingWithTest.php
@@ -1,18 +1,40 @@
 <?php
+/**
+ * Test function vipgoci_options_get_starting_with().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once( __DIR__ . './../../options.php' );
-
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class OptionsGetStartingWithTest extends TestCase {
 	/**
-	 * @covers ::vipgoci_options_get_starting_with
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
-	public function testOptionsStartingWith() {
+	protected function setUp() :void {
+		require_once __DIR__ . './../../options.php';
+	}
+
+	/**
+	 * Test common usage of the function.
+	 *
+	 * @covers ::vipgoci_options_get_starting_with
+	 *
+	 * @return void
+	 */
+	public function testOptionsStartingWith() :void {
 		$expected = array(
 			'test0' => 't0',
 			'test2' => 't9',
@@ -20,12 +42,12 @@ final class OptionsGetStartingWithTest extends TestCase {
 
 		$result = vipgoci_options_get_starting_with(
 			array(
-				'test1' => 't1',
-				'test0' => 't0',
-				'test2' => 't9',
+				'test1'  => 't1',
+				'test0'  => 't0',
+				'test2'  => 't9',
 				'atest3' => '999',
 				'atest4' => '888',
-				'atest0' => '777'
+				'atest0' => '777',
 			),
 			'test',
 			array(
