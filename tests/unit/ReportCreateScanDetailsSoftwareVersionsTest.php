05206193776fe1642f7a8b1e068239fe94b9cf6b
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 16:26:13 +0000

    Apply WP CS

--- tests/unit/ReportCreateScanDetailsSoftwareVersionsTest.php
+++ tests/unit/ReportCreateScanDetailsSoftwareVersionsTest.php
@@ -21,6 +21,8 @@ use PHPUnit\Framework\TestCase;
 final class ReportCreateScanDetailsSoftwareVersionsTest extends TestCase {
 	/**
 	 * Setup function. Require files, etc.
+	 *
+	 * @return void
 	 */
 	protected function setUp() :void {
 		require_once __DIR__ . '/../../reports.php';
@@ -33,6 +35,8 @@ final class ReportCreateScanDetailsSoftwareVersionsTest extends TestCase {
 
 	/**
 	 * Clean up options variable.
+	 *
+	 * @return void
 	 */
 	protected function tearDown() :void {
 		unset( $this->options );
@@ -42,6 +46,8 @@ final class ReportCreateScanDetailsSoftwareVersionsTest extends TestCase {
 	 * Test function with most reporting disabled.
 	 *
 	 * @covers ::vipgoci_report_create_scan_details_software_versions
+	 *
+	 * @return void
 	 */
 	public function testCreateDetails1(): void {
 		$this->options['lint']         = false;
@@ -98,6 +104,8 @@ final class ReportCreateScanDetailsSoftwareVersionsTest extends TestCase {
 	 * Test function with most reporting enabled.
 	 *
 	 * @covers ::vipgoci_report_create_scan_details_software_versions
+	 *
+	 * @return void
 	 */
 	public function testCreateDetails2(): void {
 		$this->options['lint']                   = true;
