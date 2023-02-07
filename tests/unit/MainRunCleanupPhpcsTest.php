65d0e462f0f0e7d3b165e5a62ac847e0ab0fa296
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 16:48:55 +0000

    Apply WP CS

--- tests/unit/MainRunCleanupPhpcsTest.php
+++ tests/unit/MainRunCleanupPhpcsTest.php
@@ -1,21 +1,38 @@
 <?php
+/**
+ * Test function vipgoci_run_cleanup_phpcs().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
 
-require_once __DIR__ . '/../../main.php';
-
 use PHPUnit\Framework\TestCase;
 
 /**
- * Check if PHPCS cleanup is correctly performed.
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
  */
 final class MainRunCleanupPhpcsTest extends TestCase {
+	/**
+	 * Setup function. Require files, etc.
+	 *
+	 * @return void
+	 */
+	protected function setUp() :void {
+		require_once __DIR__ . '/../../main.php';
+	}
+
 	/**
 	 * Check if PHPCS cleanup is correctly performed.
 	 *
 	 * @covers ::vipgoci_run_cleanup_phpcs
+	 *
+	 * @return void
 	 */
 	public function testRunCleanupPhpcs() :void {
 		$tmp_file_name = tempnam(
