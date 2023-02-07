f5b12f7ab28410b84dd6a1578d60efe36176d28d
gdh <gudmundur.haraldsson@automattic.com>
2023-02-07 18:08:33 +0000

    WP CS updates

--- tests/unit/MainRunInitOptionsPixelApiTest.php
+++ tests/unit/MainRunInitOptionsPixelApiTest.php
@@ -1,12 +1,13 @@
 <?php
+/**
+ * Test function vipgoci_run_init_options_pixel_api().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Unit;
-
-require_once __DIR__ . '/../../main.php';
-require_once __DIR__ . '/../../options.php';
-require_once __DIR__ . '/../../misc.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -19,13 +20,21 @@ use PHPUnit\Framework\TestCase;
 final class MainRunInitOptionsPixelApiTest extends TestCase {
 	/**
 	 * Set up variable.
+	 *
+	 * @return void
 	 */
 	protected function setUp() :void {
+		require_once __DIR__ . '/../../main.php';
+		require_once __DIR__ . '/../../options.php';
+		require_once __DIR__ . '/../../misc.php';
+
 		$this->options = array();
 	}
 
 	/**
 	 * Clean up variable.
+	 *
+	 * @return void
 	 */
 	protected function tearDown() :void {
 		unset( $this->options );
@@ -35,6 +44,8 @@ final class MainRunInitOptionsPixelApiTest extends TestCase {
 	 * Check if Pixel API default options are correctly provided.
 	 *
 	 * @covers ::vipgoci_run_init_options_pixel_api
+	 *
+	 * @return void
 	 */
 	public function testRunInitOptionsPixelApiDefault() :void {
 		vipgoci_run_init_options_pixel_api(
@@ -53,6 +64,8 @@ final class MainRunInitOptionsPixelApiTest extends TestCase {
 	 * Check if Pixel API options are correctly parsed.
 	 *
 	 * @covers ::vipgoci_run_init_options_pixel_api
+	 *
+	 * @return void
 	 */
 	public function testRunInitOptionsPixelApiCustom() :void {
 		$this->options = array(
