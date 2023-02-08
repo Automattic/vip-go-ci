7047d610cb2cd1db115b1599647203e5e8567d06
gdh <gudmundur.haraldsson@automattic.com>
2023-02-08 20:49:24 +0000

    Rename class

--- tests/integration/OptionsReadEnvTest.php
+++ tests/integration/OptionsReadEnvTest.php
@@ -4,7 +4,7 @@ require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
-final class VipgociOptionsReadEnvTest extends TestCase {
+final class OptionsReadEnvTest extends TestCase {
 	protected function setUp(): void {
 		// Add environmental variable
 		putenv(
