52f4b610de8a532bd24512eaf720c6c677c39883
gdh <gudmundur.haraldsson@automattic.com>
2023-02-08 19:06:36 +0000

    Setting variables for PHP 8.2 compatibility

--- tests/unit/OptionsSkipFolderHandleTest.php
+++ tests/unit/OptionsSkipFolderHandleTest.php
@@ -19,6 +19,13 @@ use PHPUnit\Framework\TestCase;
  */
 final class OptionsSkipFolderHandleTest extends TestCase {
 	/**
+	 * Options array.
+	 *
+	 * @var $options
+	 */
+	private array $options = array();
+
+	/**
 	 * Set variable.
 	 *
 	 * @return void
