242a37b66b132373282263a7c3cfeb32a6d240ba
gdh <gudmundur.haraldsson@automattic.com>
2023-02-09 16:46:20 +0000

    WP CS fixes

--- tests/unit/helper/CheckIrcApiAlertQueue.php
+++ tests/unit/helper/CheckIrcApiAlertQueue.php
@@ -7,7 +7,7 @@
 
 declare(strict_types=1);
 
-/*
+/**
  * Check for expected data in IRC queue.
  *
  * @param string $str_expected String to look for in the IRC queue.
@@ -21,11 +21,11 @@ function vipgoci_unittests_check_irc_api_alert_queue(
 
 	$irc_msg_queue = vipgoci_irc_api_alert_queue( null, true );
 
-	foreach( $irc_msg_queue as $irc_msg_queue_item ) {
+	foreach ( $irc_msg_queue as $irc_msg_queue_item ) {
 		if ( false !== strpos(
-				$irc_msg_queue_item,
-				$str_expected
-			) ) {
+			$irc_msg_queue_item,
+			$str_expected
+		) ) {
 			$found = true;
 		}
 	}
