e0dd73777cd12863b8d1eca87caafaf991b6be66
gdh <gudmundur.haraldsson@automattic.com>
2023-02-09 18:04:13 +0000

    PHP 8.2 compatibility update

--- tests/integration/WpscanScanSaveForSubmissionTest.php
+++ tests/integration/WpscanScanSaveForSubmissionTest.php
@@ -42,6 +42,18 @@ final class WpscanScanSaveForSubmissionTest extends TestCase {
 	);
 
 	/**
+	 * Variable for options.
+	 *
+	 * @var $options
+	 */
+	private array $options = array(
+		'git-path'        => null,
+		'github-repo-url' => null,
+		'repo-name'       => null,
+		'repo-owner'      => null,
+	);
+
+	/**
 	 * Variable for skipped files.
 	 *
 	 * @var $commit_skipped_files
