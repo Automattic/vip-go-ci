fe82ec4a5a536c364decc59b88614cd34e750808
gdh <gudmundur.haraldsson@automattic.com>
2023-02-10 17:59:09 +0000

    PHP 8.2 updates, WP CS fixes

--- tests/integration/MainRunInitGithubTokenOptionTest.php
+++ tests/integration/MainRunInitGithubTokenOptionTest.php
@@ -1,12 +1,13 @@
 <?php
+/**
+ * Test vipgoci_run_init_github_token_option().
+ *
+ * @package Automattic/vip-go-ci
+ */
 
 declare(strict_types=1);
 
 namespace Vipgoci\Tests\Integration;
-
-require_once __DIR__ . '/IncludesForTests.php';
-
-require_once __DIR__ . '/../unit/helper/IndicateTestId.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -18,9 +19,21 @@ use PHPUnit\Framework\TestCase;
  */
 final class MainRunInitGithubTokenOptionTest extends TestCase {
 	/**
+	 * Options array.
+	 *
+	 * @var $options
+	 */
+	private array $options = array();
+
+	/**
 	 * Set up variables and indication.
+	 *
+	 * @return void
 	 */
 	protected function setUp(): void {
+		require_once __DIR__ . '/IncludesForTests.php';
+		require_once __DIR__ . '/../unit/helper/IndicateTestId.php';
+
 		$this->options = array();
 
 		$this->options['github-token'] =
@@ -39,9 +52,11 @@ final class MainRunInitGithubTokenOptionTest extends TestCase {
 
 	/**
 	 * Clear variables and indication.
+	 *
+	 * @return void
 	 */
 	protected function tearDown(): void {
-		$this->options = null;
+		unset( $this->options );
 
 		// Remove the indication.
 		vipgoci_unittests_remove_indication_for_test_id( 'MainRunInitGithubTokenOptionTest' );
@@ -51,6 +66,8 @@ final class MainRunInitGithubTokenOptionTest extends TestCase {
 	 * Uses valid GitHub token to test if options are correctly processed.
 	 *
 	 * @covers ::vipgoci_run_init_github_token_option
+	 *
+	 * @return void
 	 */
 	public function testGitHubTokenValid() :void {
 		$options_test = vipgoci_unittests_options_test(
@@ -100,6 +117,8 @@ final class MainRunInitGithubTokenOptionTest extends TestCase {
 	 * Uses invalid GitHub token to test if options are correctly processed.
 	 *
 	 * @covers ::vipgoci_run_init_github_token_option
+	 *
+	 * @return void
 	 */
 	public function testGitHubTokenInvalid() :void {
 		$options_test = vipgoci_unittests_options_test(
