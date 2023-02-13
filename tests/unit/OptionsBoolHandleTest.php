73973f39fb1b1a23103d314a18422bc10575367c
gdh <gudmundur.haraldsson@automattic.com>
2023-02-13 19:30:28 +0000

    Update setUp() to be a protected function

--- tests/unit/OptionsBoolHandleTest.php
+++ tests/unit/OptionsBoolHandleTest.php
@@ -23,7 +23,7 @@ final class OptionsBoolHandleTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function setUp() :void {
+	protected function setUp() :void {
 		require_once __DIR__ . '/../../options.php';
 	}
 
