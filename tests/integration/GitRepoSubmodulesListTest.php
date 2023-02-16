cddd18d6e54587139fec0457ac4e58203ea2e9f8
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:08:10 +0000

    Apply common standard

--- tests/integration/GitRepoSubmodulesListTest.php
+++ tests/integration/GitRepoSubmodulesListTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_gitrepo_submodules_list().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class GitRepoSubmodulesListTest extends TestCase {
 	/**
 	 * Options array.
