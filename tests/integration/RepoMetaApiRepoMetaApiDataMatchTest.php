e527f07047cc80d8f4d9543dbd5bdd9c0a4a3837
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:09:49 +0000

    Apply common standard

--- tests/integration/RepoMetaApiRepoMetaApiDataMatchTest.php
+++ tests/integration/RepoMetaApiRepoMetaApiDataMatchTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_repo_meta_api_data_match().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class RepoMetaApiRepoMetaApiDataMatchTest extends TestCase {
 	/**
 	 * Options array.
