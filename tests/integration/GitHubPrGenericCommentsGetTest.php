b0710b486a53b92277022dcd682168300e162526
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:06:40 +0000

    Apply common standard

--- tests/integration/GitHubPrGenericCommentsGetTest.php
+++ tests/integration/GitHubPrGenericCommentsGetTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_github_pr_generic_comments_get().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class GitHubPrGenericCommentsGetTest extends TestCase {
 	/**
 	 * Options array.
