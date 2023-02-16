63181a6a39873937c2949b877659db12f6ce7207
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:04:51 +0000

    Apply common standard

--- tests/integration/ApFileTypesTest.php
+++ tests/integration/ApFileTypesTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_ap_file_types().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class ApFileTypesTest extends TestCase {
 	var $options_git = array(
 		'git-path'			=> null,
