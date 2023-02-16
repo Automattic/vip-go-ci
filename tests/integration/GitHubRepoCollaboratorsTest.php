1ca6479b80cff5a72b3a4689d61b93ef3683eb89
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:07:05 +0000

    Apply common standard

--- tests/integration/GitHubRepoCollaboratorsTest.php
+++ tests/integration/GitHubRepoCollaboratorsTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_github_repo_collaborators_get().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class GitHubRepoCollaboratorsTest extends TestCase {
 	/**
 	 * Options array.
