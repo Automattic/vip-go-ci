29adc5211cc0c16ae358c53d2c459969024a40ae
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:11:03 +0000

    Apply common standard

--- tests/integration/SvgScanScanSingleFileTest.php
+++ tests/integration/SvgScanScanSingleFileTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_svg_scan_single_file().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class SvgScanScanSingleFileTest extends TestCase {
 	/**
 	 * Options array.
