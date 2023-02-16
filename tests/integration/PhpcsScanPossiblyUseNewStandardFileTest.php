bf28fc2e86a61e17098d40e1856d861f0f8816d0
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:09:43 +0000

    Apply common standard

--- tests/integration/PhpcsScanPossiblyUseNewStandardFileTest.php
+++ tests/integration/PhpcsScanPossiblyUseNewStandardFileTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_phpcs_possibly_use_new_standard_file().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class PhpcsScanPossiblyUseNewStandardFileTest extends TestCase {
 	/**
 	 * Options array.
