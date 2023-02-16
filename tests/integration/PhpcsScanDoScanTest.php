4fceb1b66a516903b5ba610646a20a9b4edfee20
gdh <gudmundur.haraldsson@automattic.com>
2023-02-16 18:09:35 +0000

    Apply common standard

--- tests/integration/PhpcsScanDoScanTest.php
+++ tests/integration/PhpcsScanDoScanTest.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Test function vipgoci_phpcs_do_scan().
+ *
+ * @package Automattic/vip-go-ci
+ */
+
+declare(strict_types=1);
+
+namespace Vipgoci\Tests\Integration;
 
 require_once( __DIR__ . '/IncludesForTests.php' );
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class that implements the testing.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
 final class PhpcsScanDoScanTest extends TestCase {
 	var $options_phpcs = array(
 		'phpcs-path'		=> null,
