9761afa0aaa254d8127a53f7871b167e4aaf72e8
gdh <gudmundur.haraldsson@automattic.com>
2023-02-17 15:42:00 +0000

    Applying common standard

--- tests/integration/GitRepoDiffsFetchUnfilteredTest.php
+++ tests/integration/GitRepoDiffsFetchUnfilteredTest.php
@@ -7,24 +7,18 @@
 
 declare(strict_types=1);
 
-namespace Vipgoci\tests;
-
-require_once( __DIR__ . '/IncludesForTests.php' );
-require_once( __DIR__ . '/GitDiffsFetchUnfilteredTrait.php' );
+namespace Vipgoci\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
-/**
- * Class that implements the testing.
- *
- * @runTestsInSeparateProcesses
- * @preserveGlobalState disabled
- */
 
 /*
  * This test should be identical with GitHubDiffsFetchUnfilteredTest.php
  *
  * Note that these two tests share the same data, via
  * the GitDiffsFetchUnfilteredTrait.php file.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
  */
 final class GitRepoDiffsFetchUnfilteredTest extends TestCase {
 	/**
@@ -54,6 +48,9 @@ final class GitRepoDiffsFetchUnfilteredTest extends TestCase {
 	);
 
 	protected function setUp(): void {
+		require_once __DIR__ . '/IncludesForTests.php';
+		require_once __DIR__ . '/GitDiffsFetchUnfilteredTrait.php';
+
 		vipgoci_unittests_get_config_values(
 			'git-repo-tests',
 			$this->options_git_repo_tests
