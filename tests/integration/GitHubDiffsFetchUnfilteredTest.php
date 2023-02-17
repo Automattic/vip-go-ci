2bf4d89c82148cbc4e35f124a2354722775d5b31
gdh <gudmundur.haraldsson@automattic.com>
2023-02-17 16:14:28 +0000

    Apply common standard

--- tests/integration/GitHubDiffsFetchUnfilteredTest.php
+++ tests/integration/GitHubDiffsFetchUnfilteredTest.php
@@ -11,6 +11,8 @@ namespace Vipgoci\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 
+require_once __DIR__ . '/GitDiffsFetchUnfilteredTrait.php';
+
 /**
  * This test should be identical with GitRepoDiffsFetchUnfilteredTest.php
  *
@@ -50,7 +52,6 @@ final class GitHubDiffsFetchUnfilteredTest extends TestCase {
 
 	protected function setUp(): void {
 		require_once __DIR__ . '/IncludesForTests.php';
-		require_once __DIR__ . '/GitDiffsFetchUnfilteredTrait.php';
 
 		vipgoci_unittests_get_config_values(
 			'git-repo-tests',
