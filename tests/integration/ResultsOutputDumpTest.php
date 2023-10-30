044687ef66b6be34989420245f619a8412ffdf97
Gudmundur D. Haraldsson <gudmundur.haraldsson@automattic.com>
2023-10-30 16:08:07 +0000

    Update coding style

--- tests/integration/ResultsOutputDumpTest.php
+++ tests/integration/ResultsOutputDumpTest.php
@@ -30,7 +30,7 @@ final class ResultsOutputDumpTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	protected function setUp() :void {
+	protected function setUp(): void {
 		require_once __DIR__ . '/../../results.php';
 		require_once __DIR__ . '/../../log.php';
 
@@ -65,26 +65,26 @@ final class ResultsOutputDumpTest extends TestCase {
 			'commit'         => 'abc123',
 			'prs_implicated' => array(
 				1 => (object) array(
-					'title'       => 'testing #1',
-					'base' => (object) array(
+					'title' => 'testing #1',
+					'base'  => (object) array(
 						'ref' => 'main',
 					),
-					'head' => (object) array(
+					'head'  => (object) array(
 						'ref' => 'add/testing1',
 					),
-					'user' => (object) array(
+					'user'  => (object) array(
 						'login' => 'user1',
 					),
 				),
 				2 => (object) array(
-					'title'       => 'testing #2',
-					'base' => (object) array(
+					'title' => 'testing #2',
+					'base'  => (object) array(
 						'ref' => 'main',
 					),
-					'head' => (object) array(
+					'head'  => (object) array(
 						'ref' => 'add/testing2',
 					),
-					'user' => (object) array(
+					'user'  => (object) array(
 						'login' => 'user2',
 					),
 				),
